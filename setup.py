303b81997a1e5891aa118f5e1ea302ce41ed849d
Ulrich Petri <ulo@ulo.pe>
2016-11-11 20:48:00 +0100

    Set version to 1.0.0a0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst", "r") as readme:
     README = readme.read()
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-VERSION = '1.0.0'
+VERSION = '1.0.0a0'
 
 setup(
     name="d3a",
