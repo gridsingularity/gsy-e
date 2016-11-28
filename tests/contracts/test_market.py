d5f128f4acc7aba6d97053504b28eab4e9e04098
agatsoh <sohan.agate@gmail.com>
2016-11-28 21:08:36 +0530

    Tests for multiple Markets Successful

--- tests/contracts/test_market.py
+++ tests/contracts/test_market.py
@@ -18,9 +18,8 @@ A_key, B_key, C_key, D_key, E_key, F_key, G_key, H_key = keys[:8]
 file_dir = os.path.dirname(os.path.realpath('__file__'))
 market_contract_path = os.path.abspath(os.path.realpath(os.path.join(file_dir, MARKET_FN)))
 money_contract_path = os.path.abspath(os.path.realpath(os.path.join(file_dir, MONEY_FN)))
-
+state = tester.state()
 def make_market_contract(approver, constructor_params):
-    state = tester.state()
     market_contract_path = os.path.abspath(os.path.realpath(os.path.join(file_dir, MARKET_FN)))
     libcode = open(LIB_FN).read()
     set_address = state.contract(libcode, language='solidity', sender=tester.k0)
@@ -38,7 +37,7 @@ def print_gas_used(state, string):
 
 @pytest.fixture
 def base_state_contract():
-    state = tester.state()
+
     libcode = open(LIB_FN).read()
     logs = []
     set_address = state.contract(libcode, language='solidity', sender=tester.k0)
@@ -47,31 +46,30 @@ def base_state_contract():
                                               sender=tester.k0,
                                               constructor_parameters=[10**5, "MoneyIOU", 5, "$$"])
 
-    print("Set Adress", encode_hex(set_address))
     market_contract = state.abi_contract(None, path=market_contract_path,
                                                language='solidity',
                                                sender=tester.k0,
                                                constructor_parameters=[
                                                encode_hex(money_contract.address), 10**5, "Market", 5, "KWh"],
                                                libraries={'ItSet':encode_hex(set_address)})
-    return (state, money_contract, market_contract)
+    return (money_contract, market_contract)
 
 def test_approver(base_state_contract):
-    state, money_contract, market_contract = base_state_contract
+    money_contract, market_contract = base_state_contract
     assert money_contract.getApprover() == encode_hex(A)
 
 def test_register_market(base_state_contract):
-    state, money_contract, market_contract = base_state_contract
+    money_contract, market_contract = base_state_contract
     assert market_contract.registerMarket(1000, sender = A_key) == True
 
 def test_offer(base_state_contract):
-    state, money_contract, market_contract = base_state_contract
+    money_contract, market_contract = base_state_contract
     market_contract.registerMarket(10000, sender = A_key)
     offer_id = market_contract.offer(7, 956, sender = B_key)
     assert market_contract.getOffer(offer_id) == [7, 956, encode_hex(B)]
 
 def test_cancel(base_state_contract):
-    state, money_contract, market_contract = base_state_contract
+    money_contract, market_contract = base_state_contract
     zero_address = b'0' * 40
     assert market_contract.registerMarket(10000, sender = A_key) == True
     offer_id = market_contract.offer(7, 956, sender = B_key)
@@ -80,22 +78,25 @@ def test_cancel(base_state_contract):
     assert market_contract.getOffer(offer_id) == [0, 0, zero_address]
 
 def test_trade(base_state_contract):
-    state, money_contract, market_contract = base_state_contract
+    money_contract, market_contract = base_state_contract
     assert market_contract.registerMarket(10000, sender = A_key) == True
     offer_id = market_contract.offer(7, 956, sender = B_key)
     assert market_contract.trade(offer_id, sender = C_key) == True
     assert money_contract.balanceOf(C) == -6692
+    assert money_contract.balanceOf(B) == 6692
 
 def test_multiple_markets(base_state_contract):
-    state, money_contract, market_contract_one = base_state_contract
-    market_contract_two = make_market_contract(tester.k0, [encode_hex(money_contract.address),
-                                                10**5, "Market2", 0, "KWh"])
-    print("Market one ==", encode_hex(market_contract_one.address))
-    print("Market two==",encode_hex(market_contract_two.address))
-    print(market_contract_one.registerMarket(10000, sender = tester.k0))
-    print("Approver==", money_contract.getApprover())
-    assert encode_hex(money_contract.address) == market_contract_two.getMoneyIOUAddress()
-    # MoneyIOU gets the same address as market two
-    print("MoneyIOU ==",market_contract_two.getMoneyIOUAddress())
-    # market_contract_two.registerMarket(20000, sender = tester.k0)
-    print(money_contract.getApprovedMarkets())
+    money_contract, market_contract_a = base_state_contract
+    market_contract_b = make_market_contract(tester.k0, [encode_hex(money_contract.address),
+                                                10**5, "Market2", 5, "KWh"])
+    assert encode_hex(market_contract_a.address) != encode_hex(market_contract_b.address)
+    assert market_contract_a.registerMarket(10000, sender = tester.k0) == True
+    assert market_contract_b.registerMarket(20000, sender = tester.k0) == True
+    offerid_a = market_contract_a.offer(8, 790, sender = B_key)
+    offerid_b = market_contract_b.offer(9, 899, sender = C_key)
+    assert market_contract_a.trade(offerid_a, sender = D_key) == True
+    assert market_contract_b.trade(offerid_b, sender = E_key) == True
+    assert money_contract.balanceOf(D) == -6320
+    assert money_contract.balanceOf(B) == 6320
+    assert money_contract.balanceOf(E) == -8091
+    assert money_contract.balanceOf(C) == 8091
