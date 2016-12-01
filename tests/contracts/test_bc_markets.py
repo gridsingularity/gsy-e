ef22a1e7ebf902deb74c8d7d71cf0224e9118c25
agatsoh <sohan.agate@gmail.com>
2016-12-01 15:04:47 +0530

    Correcting flake8 errors for tests

--- tests/contracts/test_bc_markets.py
+++ tests/contracts/test_bc_markets.py
@@ -1,8 +1,8 @@
 from contextlib import contextmanager
 from ethereum import tester
 from ethereum.utils import encode_hex
-import ethereum.abi as abi
-import pytest, pprint, os
+import pytest
+import os
 
 MARKET_FN = '../../src/d3a/contracts/Market.sol'
 LIB_FN = '../../src/d3a/contracts/byte_set_lib.sol'
@@ -11,7 +11,7 @@ MONEY_FN = '../../src/d3a/contracts/MoneyIOU.sol'
 # setup accounts
 accounts = tester.accounts
 keys = tester.keys
-A, B, C, D, E, F, G, H= accounts[:8]
+A, B, C, D, E, F, G, H = accounts[:8]
 A_key, B_key, C_key, D_key, E_key, F_key, G_key, H_key = keys[:8]
 
 # contract paths
@@ -19,15 +19,19 @@ file_dir = os.path.dirname(os.path.realpath('__file__'))
 market_contract_path = os.path.abspath(os.path.realpath(os.path.join(file_dir, MARKET_FN)))
 money_contract_path = os.path.abspath(os.path.realpath(os.path.join(file_dir, MONEY_FN)))
 state = tester.state()
+
+
 def make_market_contract(approver, constructor_params):
     market_contract_path = os.path.abspath(os.path.realpath(os.path.join(file_dir, MARKET_FN)))
     libcode = open(LIB_FN).read()
     set_address = state.contract(libcode, language='solidity', sender=tester.k0)
     return state.abi_contract(None, path=market_contract_path,
-                                    language='solidity',
-                                    sender=approver,
-                                    constructor_parameters = constructor_params,
-                                    libraries={'ItSet':encode_hex(set_address)})
+                              language='solidity',
+                              sender=approver,
+                              constructor_parameters=constructor_params,
+                              libraries={'ItSet': encode_hex(set_address)})
+
+
 @contextmanager
 def print_gas_used(state, string):
     t_gas = state.block.gas_used
@@ -35,62 +39,71 @@ def print_gas_used(state, string):
     gas_used = state.block.gas_used - t_gas - 21000
     print(string, gas_used)
 
+
 @pytest.fixture
 def base_state_contract():
 
     libcode = open(LIB_FN).read()
-    logs = []
     set_address = state.contract(libcode, language='solidity', sender=tester.k0)
     money_contract = state.abi_contract(None, path=money_contract_path,
-                                              language='solidity',
-                                              sender=tester.k0,
-                                              constructor_parameters=[10**5, "MoneyIOU", 5, "$$"])
+                                        language='solidity',
+                                        sender=tester.k0,
+                                        constructor_parameters=[10**5, "MoneyIOU", 5, "$$"])
 
     market_contract = state.abi_contract(None, path=market_contract_path,
-                                               language='solidity',
-                                               sender=tester.k0,
-                                               constructor_parameters=[
-                                               encode_hex(money_contract.address), 10**5, "Market", 5, "KWh", 3*60],
-                                               libraries={'ItSet':encode_hex(set_address)})
+                                         language='solidity',
+                                         sender=tester.k0,
+                                         constructor_parameters=[
+                                            encode_hex(money_contract.address),
+                                            10**5, "Market", 5,
+                                            "KWh", 3*60],
+                                         libraries={'ItSet': encode_hex(set_address)})
     return (money_contract, market_contract)
+
 
 def test_approver(base_state_contract):
     money_contract, market_contract = base_state_contract
     assert money_contract.getApprover() == encode_hex(A)
 
+
 def test_register_market(base_state_contract):
     money_contract, market_contract = base_state_contract
-    assert market_contract.registerMarket(1000, sender = A_key) == True
+    assert market_contract.registerMarket(1000, sender=A_key)
+
 
 def test_offer(base_state_contract):
     money_contract, market_contract = base_state_contract
-    market_contract.registerMarket(10000, sender = A_key)
-    offer_id = market_contract.offer(7, 956, sender = B_key)
+    market_contract.registerMarket(10000, sender=A_key)
+    offer_id = market_contract.offer(7, 956, sender=B_key)
     assert market_contract.getOffer(offer_id) == [7, 956, encode_hex(B)]
+
 
 def test_cancel(base_state_contract):
     money_contract, market_contract = base_state_contract
     zero_address = b'0' * 40
-    assert market_contract.registerMarket(10000, sender = A_key) == True
-    offer_id = market_contract.offer(7, 956, sender = B_key)
+    assert market_contract.registerMarket(10000, sender=A_key)
+    offer_id = market_contract.offer(7, 956, sender=B_key)
     assert market_contract.getOffer(offer_id) == [7, 956, encode_hex(B)]
-    assert market_contract.cancel(offer_id, sender = B_key) == True
+    assert market_contract.cancel(offer_id, sender=B_key)
     assert market_contract.getOffer(offer_id) == [0, 0, zero_address]
+
 
 def test_trade(base_state_contract):
     money_contract, market_contract = base_state_contract
-    assert market_contract.registerMarket(10000, sender = A_key) == True
-    offer_id = market_contract.offer(7, 956, sender = B_key)
-    assert market_contract.trade(offer_id, sender = C_key) == True
+    assert market_contract.registerMarket(10000, sender=A_key)
+    offer_id = market_contract.offer(7, 956, sender=B_key)
+    assert market_contract.trade(offer_id, sender=C_key)
     assert money_contract.balanceOf(C) == -6692
     assert money_contract.balanceOf(B) == 6692
+
 
 def test_seller_trade_fail(base_state_contract):
     """Checks if msg.sender == offer.seller then trade should fail"""
     money_contract, market_contract = base_state_contract
-    assert market_contract.registerMarket(10000, sender = A_key) == True
-    offer_id = market_contract.offer(7, 956, sender = B_key)
-    assert market_contract.trade(offer_id, sender = B_key) == False
+    assert market_contract.registerMarket(10000, sender=A_key)
+    offer_id = market_contract.offer(7, 956, sender=B_key)
+    assert not market_contract.trade(offer_id, sender=B_key)
+
 
 def test_offer_price_negative(base_state_contract):
     """
@@ -98,55 +111,60 @@ def test_offer_price_negative(base_state_contract):
     buyer to take your excess energy. Difference is offer price is negative.
     """
     money_contract, market_contract = base_state_contract
-    assert market_contract.registerMarket(10000, sender = A_key) == True
+    assert market_contract.registerMarket(10000, sender=A_key)
     buyer, buyer_key, seller, seller_key = B, B_key, C, C_key
-    offer_id = market_contract.offer(7, -10, sender = seller_key)
-    assert market_contract.trade(offer_id, sender = buyer_key) == True
+    offer_id = market_contract.offer(7, -10, sender=seller_key)
+    assert market_contract.trade(offer_id, sender=buyer_key)
     assert money_contract.balanceOf(buyer) == 70
     assert money_contract.balanceOf(seller) == -70
 
+
 def test_multiple_markets(base_state_contract):
     money_contract, market_contract_a = base_state_contract
-    market_contract_b = make_market_contract(tester.k0, [encode_hex(money_contract.address),
-                                                10**5, "Market2", 5, "KWh", 4*60])
+    market_contract_b = make_market_contract(tester.k0, [
+                            encode_hex(money_contract.address),
+                            10**5, "Market2", 5,
+                            "KWh", 4*60])
     assert encode_hex(market_contract_a.address) != encode_hex(market_contract_b.address)
-    assert market_contract_a.registerMarket(10000, sender = tester.k0) == True
-    assert market_contract_b.registerMarket(20000, sender = tester.k0) == True
-    offerid_a = market_contract_a.offer(8, 790, sender = B_key)
-    offerid_b = market_contract_b.offer(9, 899, sender = C_key)
-    assert market_contract_a.trade(offerid_a, sender = D_key) == True
-    assert market_contract_b.trade(offerid_b, sender = E_key) == True
+    assert market_contract_a.registerMarket(10000, sender=tester.k0)
+    assert market_contract_b.registerMarket(20000, sender=tester.k0)
+    offerid_a = market_contract_a.offer(8, 790, sender=B_key)
+    offerid_b = market_contract_b.offer(9, 899, sender=C_key)
+    assert market_contract_a.trade(offerid_a, sender=D_key)
+    assert market_contract_b.trade(offerid_b, sender=E_key)
     assert money_contract.balanceOf(D) == -6320
     assert money_contract.balanceOf(B) == 6320
     assert money_contract.balanceOf(E) == -8091
     assert money_contract.balanceOf(C) == 8091
 
+
 def test_getOffersIdsBelow(base_state_contract):
     money_contract, market_contract = base_state_contract
-    assert market_contract.registerMarket(10000, sender = A_key) == True
-    offerid_a = market_contract.offer(7, 956, sender = B_key)
+    assert market_contract.registerMarket(10000, sender=A_key)
+    offerid_a = market_contract.offer(7, 956, sender=B_key)
     assert market_contract.getOffer(offerid_a) == [7, 956, encode_hex(B)]
-    offerid_b = market_contract.offer(8, 799, sender = C_key)
+    offerid_b = market_contract.offer(8, 799, sender=C_key)
     assert market_contract.getOffer(offerid_b) == [8, 799, encode_hex(C)]
-    offerid_c = market_contract.offer(9, 655, sender = D_key)
+    offerid_c = market_contract.offer(9, 655, sender=D_key)
     assert market_contract.getOffer(offerid_c) == [9, 655, encode_hex(D)]
-    offerid_d = market_contract.offer(11, 1024, sender = E_key)
+    offerid_d = market_contract.offer(11, 1024, sender=E_key)
     assert market_contract.getOffer(offerid_d) == [11, 1024, encode_hex(E)]
     assert market_contract.getOffersIdsBelow(1000) == [offerid_a, offerid_b, offerid_c]
 
+
 def test_trade_fail_afterinterval(base_state_contract):
     money_contract, market_contract = base_state_contract
-    market_contract.registerMarket(10000, sender = A_key)
-    offerid_a = market_contract.offer(7, 956, sender = B_key)
+    market_contract.registerMarket(10000, sender=A_key)
+    offerid_a = market_contract.offer(7, 956, sender=B_key)
     assert market_contract.getOffer(offerid_a) == [7, 956, encode_hex(B)]
-    offerid_b = market_contract.offer(8, 799, sender = C_key)
+    offerid_b = market_contract.offer(8, 799, sender=C_key)
     assert market_contract.getOffer(offerid_b) == [8, 799, encode_hex(C)]
     state.block.timestamp += 2*60
-    assert market_contract.trade(offerid_a, sender = D_key) == True
+    assert market_contract.trade(offerid_a, sender=D_key)
     assert money_contract.balanceOf(D) == -6692
     assert money_contract.balanceOf(B) == 6692
     # making block.timestamp more than the interval of the market_contract
     state.block.timestamp += 4*60
-    assert market_contract.trade(offerid_b, sender = E_key) == False
+    assert not market_contract.trade(offerid_b, sender=E_key)
     assert money_contract.balanceOf(C) == 0
     assert money_contract.balanceOf(E) == 0
--- tests/contracts/test_ioutoken.py
+++ tests/contracts/test_ioutoken.py
@@ -1,7 +1,7 @@
 from contextlib import contextmanager
 from ethereum import tester
-import ethereum.abi as abi
-import pytest, pprint, os
+import pytest
+import os
 
 CONTRACT_FN = '../../src/d3a/contracts/IOUToken.sol'
 
@@ -12,12 +12,14 @@ keys = tester.keys
 A, B, C, D, E = accounts[:5]
 A_key, B_key, C_key, D_key, E_key = keys[:5]
 
+
 @contextmanager
 def print_gas_used(state, string):
     t_gas = state.block.gas_used
     yield
     gas_used = state.block.gas_used - t_gas - 21000
     print(string, gas_used)
+
 
 @pytest.fixture
 def base_state_contract():
@@ -26,30 +28,31 @@ def base_state_contract():
     filename = os.path.join(file_dir, CONTRACT_FN)
     filename = os.path.abspath(os.path.realpath(filename))
     logs = []
-    contract = state.abi_contract(None, path=filename,
-                                        language='solidity',
-                                        sender=tester.k0,
-                                        constructor_parameters=[10**5, "Testcoin", 5, "$$"])
+    contract = state.abi_contract(None, path=filename, language='solidity', sender=tester.k0,
+                                  constructor_parameters=[10**5, "Testcoin", 5, "$$"])
     return (state, contract, logs)
+
 
 def test_balanceOf(base_state_contract):
     state, contract, logs = base_state_contract
     assert contract.balanceOf(A) == 10**5
 
+
 def test_transfer(base_state_contract):
     state, contract, logs = base_state_contract
-    contract.transfer(B, 10000, sender = A_key)
+    contract.transfer(B, 10000, sender=A_key)
     assert contract.balanceOf(B) == 10000
     assert contract.balanceOf(A) == 90000
 
 
 def test_transfer_negative_balance(base_state_contract):
     state, contract, logs = base_state_contract
-    assert contract.transfer(B, 10000, sender = A_key)
+    assert contract.transfer(B, 10000, sender=A_key)
     assert contract.balanceOf(B) == 10000
-    contract.transfer(C, 11000, sender = B_key)
+    contract.transfer(C, 11000, sender=B_key)
     assert contract.balanceOf(B) == -1000
     assert contract.balanceOf(C) == 11000
+
 
 def test_overflows(base_state_contract):
     state, contract, logs = base_state_contract
@@ -57,21 +60,23 @@ def test_overflows(base_state_contract):
     assert contract.balanceOf(C) == 0
     higher_range = 2**255-1
     lower_range = -2**255
-    contract.transfer(B, higher_range, sender = C_key)
-    assert contract.balanceOf(B) == higher_range # positive value
-    contract.transfer(B, 1, sender = D_key) # units fold at this point
-    assert contract.balanceOf(B) == lower_range # negative value
-    assert contract.balanceOf(C) == lower_range + 1 # negative value
-    contract.transfer(A, 2, sender = C_key) # units fold at this point
-    assert contract.balanceOf(C) == higher_range # positive value
+    contract.transfer(B, higher_range, sender=C_key)
+    assert contract.balanceOf(B) == higher_range  # positive value
+    contract.transfer(B, 1, sender=D_key)  # units fold at this point
+    assert contract.balanceOf(B) == lower_range  # negative value
+    assert contract.balanceOf(C) == lower_range + 1  # negative value
+    contract.transfer(A, 2, sender=C_key)  # units fold at this point
+    assert contract.balanceOf(C) == higher_range  # positive value
+
 
 def test_approve_allowance(base_state_contract):
     state, contract, logs = base_state_contract
-    contract.approve(C, 1000, sender = A_key)
+    contract.approve(C, 1000, sender=A_key)
     assert contract.allowance(A, C) == 1000
+
 
 def test_transferFrom(base_state_contract):
     state, contract, logs = base_state_contract
-    contract.approve(C, 1000, sender = A_key)
-    contract.transferFrom(A, B, 500, sender = C_key)
+    contract.approve(C, 1000, sender=A_key)
+    contract.transferFrom(A, B, 500, sender=C_key)
     assert contract.balanceOf(B) == 500
