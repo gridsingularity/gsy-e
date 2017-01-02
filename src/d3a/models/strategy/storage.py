22ef676e60d86e58d232a0907139b1ceac8552ac
Oliver Bünting <oliver@brainbot.com>
2017-01-02 19:40:37 +0100

    Fixed buying and selling behaviour of storage

--- src/d3a/models/strategy/storage.py
+++ src/d3a/models/strategy/storage.py
@@ -16,6 +16,7 @@ class StorageStrategy(BaseStrategy):
         self.used_storage = 0.00
         self.blocked_storage = 0.00
         self.offer_price = 10000
+        self.selling_price = 30
 
     def event_tick(self, *, area):
         avg_cheapest_offer_price = self.find_avg_cheapest_offers()
@@ -25,7 +26,7 @@ class StorageStrategy(BaseStrategy):
             for offer in market.sorted_offers:
                 if (
                         self.used_storage + self.blocked_storage + offer.energy <= STORAGE_CAPACITY
-                        and (offer.price / offer.energy) < avg_cheapest_offer_price
+                        and (offer.price / offer.energy) <= avg_cheapest_offer_price
                 ):
                     try:
                         self.accept_offer(market, offer)
@@ -34,55 +35,67 @@ class StorageStrategy(BaseStrategy):
                     except MarketException:
                         # Offer already gone etc., try next one.
                         continue
-                        # TODO check if posted offers are still the cheapest
+                # TODO exit all of the loops correctly
+                # TODO - why isn't the event_tick code executed every tick properly?
+        self.sell_energy()
 
     def event_market_cycle(self):
         # Update the energy balances
         past_market = list(self.area.past_markets.values())[-1]
-        avg_cheapest_offer_price = self.find_avg_cheapest_offers()
-        # Maximum selling price using the highest risk is 20% above the average price
-        max_selling_price = 0.2 * avg_cheapest_offer_price
-        # formula to calculate a profitable selling price
-        selling_price = avg_cheapest_offer_price + ((self.risk / 100) * max_selling_price)
         # if energy for this slot was bought: sell it in the most expensive market
         for bought in self.done_trades[past_market]:
             self.blocked_storage -= bought.energy
             self.used_storage += bought.energy
-        expensive_offers = list(self.area.cheapest_offers)[-1]
-        # Finding the most expensive market
-        market = expensive_offers.market
-        # Post offer in most expensive market
-        #        self.log.info("selling_price %s", selling_price)
-        #        self.log.info("max_selling_price %s", max_selling_price)
-        self.log.info("selling_price %s", selling_price)
-        self.log.info("list(market.sorted_offers)[0].price %s",
-                      list(market.sorted_offers)[0].price)
-        self.log.info("self.used_storage  %s", self.used_storage)
-        if (
-            selling_price < list(market.sorted_offers)[0].price and
-            self.used_storage > 0 and
-            # if selling price cheaper than the price of currently existing offer
-            selling_price < self.offer_price
-        ):
-            # TODO Here we need to delete the old offers to prevent trying to spend the energy
-            # several times
-            offer = market.offer(
-                self.used_storage,
-                selling_price,
-                self.owner.name
-            )
-            self.offers_posted[offer.id] = market
-            self.offer_price = offer.price
+        self.sell_energy()
 
     def event_trade(self, *, market, trade):
         if self.owner.name == trade.seller:
             self.used_storage -= trade.offer.energy
             # TODO post information about earned money
 
+    def sell_energy(self):
+        avg_cheapest_offer_price = self.find_avg_cheapest_offers()
+        # Maximum selling price using the highest risk is 20% above the average price
+        max_selling_price = 0.2 * avg_cheapest_offer_price
+        # formula to calculate a profitable selling price
+        new_selling_price = avg_cheapest_offer_price + ((self.risk / 100) * max_selling_price)
+        expensive_offers = list(self.area.cheapest_offers)[-1]
+        # Finding the most expensive market
+        market = expensive_offers.market
+        sorted_offer_price = (
+            list(market.sorted_offers)[0].price /
+            list(market.sorted_offers)[0].energy
+        )
+        # Post offer in most expensive market
+        self.log.info("new_selling_price %s", new_selling_price)
+        self.log.info("sorted_offer_price %s", sorted_offer_price)
+        self.log.info("self.selling_price %s", self.selling_price)
+        if (
+                new_selling_price <= sorted_offer_price and
+                self.used_storage > 0 and
+                new_selling_price < self.selling_price
+        ):
+            # TODO Here we need to delete the old offers to prevent trying to spend the energy
+            # several times
+            # Deleting all old offers
+            for market in self.offers_posted.values():
+                for offer_id in self.offers_posted.keys():
+                    market.delete_offer(offer_id)
+            # Posting offer with new information
+            offer = market.offer(
+                self.used_storage,
+                new_selling_price * self.used_storage,
+                self.owner.name
+            )
+            # Updating parameters
+            self.selling_price = new_selling_price
+            self.offers_posted[offer.id] = market
+            self.offer_price = offer.price
+
     def find_avg_cheapest_offers(self):
         cheapest_offers = self.area.cheapest_offers
         avg_cheapest_offer_price = (
-            sum(offer.price for offer in cheapest_offers)
+            sum((offer.price / offer.energy) for offer in cheapest_offers)
             / max(len(cheapest_offers), 1)
         )
         return avg_cheapest_offer_price
