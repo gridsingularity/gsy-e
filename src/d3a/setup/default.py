3ba6ee9b7d38ed541d18d01fa9291c26d35409d2
Oliver Bünting <oliver@brainbot.com>
2017-01-23 19:41:51 +0100

    Added mocked storage to Street 1

--- src/d3a/setup/default.py
+++ src/d3a/setup/default.py
@@ -6,8 +6,8 @@ from d3a.models.appliance.simple import SimpleAppliance
 from d3a.models.area import Area
 from d3a.models.strategy.commercial_producer import CommercialStrategy
 from d3a.models.strategy.fridge import FridgeStrategy
+from d3a.models.strategy.greedy_night_storage import NightStorageStrategy
 from d3a.models.strategy.heatpump import HeatPumpStrategy
-from d3a.models.strategy.mocked_storage import NightStorageStrategy
 from d3a.models.strategy.permanent import PermanentLoadStrategy
 from d3a.models.strategy.pv import PVStrategy
 from d3a.models.strategy.storage import StorageStrategy
