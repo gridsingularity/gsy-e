990e56366c8127372b54c8e70537c1c263aef329
Ulrich Petri <ulo@ulo.pe>
2017-02-12 15:24:51 +0100

    Cache slot energy in SimpleAppliance

--- src/d3a/models/appliance/simple.py
+++ src/d3a/models/appliance/simple.py
@@ -3,6 +3,11 @@ from d3a.models.appliance.base import BaseAppliance
 
 class SimpleAppliance(BaseAppliance):
     """Example appliance that reports the traded energy in increments each tick"""
+
+    def __init__(self):
+        super().__init__()
+        self._market_energy = {}
+
     def event_tick(self, *, area):
         if not self.owner:
             # Should not happen
@@ -12,6 +17,8 @@ class SimpleAppliance(BaseAppliance):
             # No current market yet
             return
         # Fetch traded energy for `market`
-        energy = self.owner.strategy.energy_balance(market)
+        energy = self._market_energy.get(market)
+        if energy is None:
+            energy = self._market_energy[market] = self.owner.strategy.energy_balance(market)
         if energy:
             area.report_accounting(market, self.owner.name, energy / area.config.ticks_per_slot)
