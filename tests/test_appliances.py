c116df5c1ebd46eb586c91acac946045cac3d9f2
Oliver Bünting <oliver@brainbot.com>
2017-08-18 15:40:07 +0200

    optimized imports

--- tests/test_appliances.py
+++ tests/test_appliances.py
@@ -1,8 +1,8 @@
 from copy import deepcopy
+
 import pytest
 
 from d3a.models.appliance.fridge import FridgeAppliance
-from d3a.models.appliance.properties import ElectricalProperties
 from d3a.models.appliance.pv import PVAppliance
 from d3a.models.appliance.switchable import SwitchableAppliance
 from d3a.models.area import DEFAULT_CONFIG
@@ -10,12 +10,12 @@ from d3a.models.strategy.const import MAX_FRIDGE_TEMP
 
 
 class FakeSwitchableStrategy:
-    def energy_balance(self,_):
+    def energy_balance(self, _):
         return 5
 
 
 class FakePVStrategy:
-    def energy_balance(self,_):
+    def energy_balance(self, _):
         return 1
 
     def __init__(self):
@@ -29,7 +29,7 @@ class FakeOwner:
 
 
 class FakeOwnerWithStrategy(FakeOwner):
-    def __init__(self,strategy):
+    def __init__(self, strategy):
         self._strategy = strategy
 
     @property
@@ -42,7 +42,7 @@ class FakeArea:
         self.reported_value = None
         self.is_nighttime = False
 
-    def report_accounting(self,market,owner,value):
+    def report_accounting(self, market, owner, value):
         self.reported_value = value
 
     @property
@@ -53,7 +53,7 @@ class FakeArea:
     def current_market(self):
         return "market"
 
-    def set_nighttime(self,is_nighttime):
+    def set_nighttime(self, is_nighttime):
         self.is_nighttime = is_nighttime
 
     @property
@@ -61,7 +61,7 @@ class FakeArea:
         if self.is_nighttime:
             return 4
         else:
-            return int(36000/DEFAULT_CONFIG.tick_length.in_seconds())
+            return int(36000 / DEFAULT_CONFIG.tick_length.in_seconds())
             # 10 AM so the output of the PV is >0
 
 
@@ -86,8 +86,9 @@ def test_fridge_appliance_door(fridge_fixture):
 # reports traded surplus energy
 
 def test_fridge_appliance_report_energy_surplus(fridge_fixture):
-    fridge_fixture.report_energy(10)
-    assert fridge_fixture.area.reported_value>0
+    test_energy = 10
+    fridge_fixture.report_energy(test_energy)
+    assert fridge_fixture.area.reported_value > 0
 
 
 # no report if there is no surplus or deficit
@@ -109,13 +110,12 @@ def test_fridge_appliance_heats_up_when_open(fridge_fixture):
     assert open_fridge.temperature > fridge_fixture.temperature
 
 
-
 # always buys energy if we have none and upper temperature constraint is violated
 
 def test_fridge_appliance_report_energy_too_warm(fridge_fixture):
-    fridge_fixture.temperature = MAX_FRIDGE_TEMP+1
+    fridge_fixture.temperature = MAX_FRIDGE_TEMP + 1
     fridge_fixture.report_energy(0)
-    assert fridge_fixture.area.reported_value<0
+    assert fridge_fixture.area.reported_value < 0
 
 
 @pytest.fixture
@@ -172,9 +172,9 @@ def test_pv_appliance_has_energy_by_day(pv_fixture):
 # has energy at all cloud cover percentages except 100%
 
 def test_pv_appliance_cloud_cover(pv_fixture):
-    pv_fixture.fire_trigger("cloud_cover",percent=100.0,duration=10)
+    pv_fixture.fire_trigger("cloud_cover", percent=100.0, duration=10)
     pv_fixture.event_tick(area=pv_fixture.area)
     assert pv_fixture.area.reported_value is None
-    pv_fixture.fire_trigger("cloud_cover",percent=90.0,duration=10)
+    pv_fixture.fire_trigger("cloud_cover", percent=95.0, duration=10)
     pv_fixture.event_tick(area=pv_fixture.area)
-    assert pv_fixture.area.reported_value>0
+    assert pv_fixture.area.reported_value > 0
