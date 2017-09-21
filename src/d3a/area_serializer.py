a0ba89d090b762d184665ba75332e74e640346c3
Jochen Müller <mail@jnmr.de>
2017-09-21 15:37:11 +0200

    add strategies and appliances to serialization (unparametrized)

--- src/d3a/area_serializer.py
+++ src/d3a/area_serializer.py
@@ -1,12 +1,21 @@
 import json
 
 from d3a.models.area import Area
+from d3a.models.strategy.base import BaseStrategy
+from d3a.models.appliance.simple import SimpleAppliance
+
+from d3a.models.appliance.fridge import FridgeAppliance  # NOQA
+from d3a.models.strategy.fridge import FridgeStrategy  # NOQA
 
 
 class AreaEncoder(json.JSONEncoder):
     def default(self, obj):
         if type(obj) is Area:
             return self._encode_area(obj)
+        elif isinstance(obj, BaseStrategy):
+            return self._encode_strategy(obj)
+        elif isinstance(obj, SimpleAppliance):
+            return self._encode_appliance(obj)
 
     def _encode_area(self, area):
         result = {"name": area.name}
@@ -18,10 +27,31 @@ class AreaEncoder(json.JSONEncoder):
             result['appliance'] = area.appliance
         return result
 
+    def _encode_strategy(self, strategy):
+        result = {"type": strategy.__class__.__name__}
+        # TODO parameters
+        return result
+
+    def _encode_appliance(self, appliance):
+        result = {"type": appliance.__class__.__name__}
+        return result
+
 
 def area_to_string(area):
     """Create a json string representation of an Area"""
     return json.dumps(area, cls=AreaEncoder)
+
+
+def _instance_from_dict(description):
+    args = description['args'] if 'args' in description else list()
+    kwargs = description['kwargs'] if 'kwargs' in description else dict()
+    try:
+        return globals()[description['type']](*args, **kwargs)
+    except Exception as exception:
+        if 'type' in description and type(exception) is KeyError:
+            raise ValueError("Unknown class '%s'" % description['type'])
+        else:
+            raise exception
 
 
 def area_from_dict(description):
@@ -33,10 +63,16 @@ def area_from_dict(description):
                 children.append(area_from_dict(child))
         else:
             children = None
-        strategy = None  # TODO
-        appliance = None  # TODO
+        if 'strategy' in description:
+            strategy = _instance_from_dict(description['strategy'])
+        else:
+            strategy = None
+        if 'appliance' in description:
+            appliance = _instance_from_dict(description['appliance'])
+        else:
+            appliance = None
         return Area(name, children, strategy, appliance)
-    except (json.JSONDecodeError, KeyError, TypeError) as error:
+    except (json.JSONDecodeError, KeyError, TypeError, ValueError) as error:
         raise ValueError("Input is not a valid area description (%s)" % str(error))
 
 
--- tests/test_area_serializer.py
+++ tests/test_area_serializer.py
@@ -1,8 +1,13 @@
-from d3a.models.area import Area
+import json
+import pytest
+
 from d3a.area_serializer import area_to_string, area_from_string
+from d3a.models.appliance.fridge import FridgeAppliance
+from d3a.models.area import Area
+from d3a.models.strategy.fridge import FridgeStrategy
 
 
-def test_area_with_children():
+def test_area_with_children_roundtrip():
     child1 = Area("child1")
     child2 = Area("child2")
     parent = Area("parent", [child1, child2])
@@ -11,3 +16,23 @@ def test_area_with_children():
     assert recovered.name == "parent"
     assert recovered.children[0].name == "child1"
     assert recovered.children[1].name == "child2"
+
+
+def test_encode_strategy_appliance():
+    area = Area("child", [], FridgeStrategy(), FridgeAppliance())
+    area_dict = json.loads(area_to_string(area))
+    assert 'children' not in area_dict
+    assert area_dict['strategy']['type'] == 'FridgeStrategy'
+    assert area_dict['appliance']['type'] == 'FridgeAppliance'
+
+
+def test_strategy_appliance_roundtrip():
+    area = Area("child", [], FridgeStrategy(), FridgeAppliance())
+    recovered = area_from_string(area_to_string(area))
+    assert type(recovered.strategy) is FridgeStrategy
+    assert type(recovered.appliance) is FridgeAppliance
+
+
+def test_raises_unknown_class():
+    with pytest.raises(ValueError):
+        area_from_string("{'name':'broken','strategy':'NonexistentStrategy'}")
