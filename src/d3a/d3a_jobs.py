cebee3bebffebe08eb730fa0e7722612a65988e5
Jochen Müller <mail@jnmr.de>
2017-10-04 17:49:00 +0200

    use settings provided by d3a-web

--- src/d3a/d3a_jobs.py
+++ src/d3a/d3a_jobs.py
@@ -1,5 +1,6 @@
 import logging
-from pendulum.interval import Interval
+import pendulum.interval as interval
+from datetime import timedelta
 from rq import get_current_job
 from rq.decorators import job
 
@@ -8,8 +9,8 @@ from d3a.simulation import Simulation
 from d3a.web import start_web
 
 
-@job('default', timeout='60s')
-def start():
+@job('default')
+def start(scenario, settings):
     logging.getLogger().setLevel(logging.ERROR)
     interface = "0.0.0.0"
     port = 5000
@@ -19,13 +20,18 @@ def start():
     job.meta['api_url'] = api_url
     job.save_meta()
 
+    if settings is None:
+        settings = {}
+
     config = SimulationConfig(
-        duration=Interval(hours=12),
-        slot_length=Interval(minutes=15),
-        tick_length=Interval(seconds=15),
-        market_count=4
+        duration=interval.instance(settings.get('duration', timedelta(days=1))),
+        slot_length=interval.instance(settings.get('slot_length', timedelta(minutes=15))),
+        tick_length=interval.instance(settings.get('tick_length', timedelta(seconds=1))),
+        market_count=settings.get('market_count', 4)
     )
 
-    simulation = Simulation('default', config, api_url=api_url)
+    simulation = Simulation('default', config, api_url=api_url,
+                            slowdown=settings.get('slowdown', 0))
+
     start_web(interface, port, simulation)
     simulation.run()
