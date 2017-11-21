51f7da44fc672d24f17b06c1df303d7e5b1ba3ba
Ulrich Petri <ulo@ulo.pe>
2017-11-21 16:39:17 +0100

    Spread out FB buying behaviour over specified hours

--- src/d3a/models/strategy/facebook_device.py
+++ src/d3a/models/strategy/facebook_device.py
@@ -1,15 +1,15 @@
-import pendulum
-from typing import Dict, Any  # noqa
-from collections import OrderedDict
-from d3a.exceptions import D3AException
+import random
+
 from first import first
+from pendulum.interval import Interval
 
 from d3a.exceptions import MarketException
 from d3a.models.strategy.base import BaseStrategy
 
 
 class FacebookDeviceStrategy(BaseStrategy):
-    def __init__(self, avg_power, hrs_per_day=None, hrs_of_day=(0, 23), consolidated_cycle=None):
+    def __init__(self, avg_power, hrs_per_day=None, hrs_of_day=(0, 23), consolidated_cycle=None,
+                 random_factor=0):
         super().__init__()
         self.avg_power = avg_power  # Average power in watts
         if not hrs_per_day and not consolidated_cycle:
@@ -19,32 +19,49 @@ class FacebookDeviceStrategy(BaseStrategy):
         self.consolidated_cycle = consolidated_cycle
         self.daily_energy_required = self.calculate_daily_energy_req()
         # Energy consumed during the day ideally should not exceed daily_energy_required
-        self.energy_consumed = OrderedDict()  # type: Dict[Pendulum, float]
-        self.midnight = None
+        self.energy_bought_in_slot = 0
+        self.energy_missing = 0
+        self.energy_per_slot = None
+        self.random_factor = random_factor
         # This is the minimum batch of energy that a device buys, please check whether this could
         # be a parameter on the constructor or if we want to deal in percentages
         self.min_energy_buy = 50  # 50 wh is the energy to buy each time for the device
         self.hrs_of_day = hrs_of_day
-        self.period_active = self.calculate_active_period()
+        active_hours_count = (hrs_of_day[1] - hrs_of_day[0] + 1)
+        if hrs_per_day > active_hours_count:
+            raise ValueError(
+                "Device can't be active more hours per day than active hours: {} > {}".format(
+                    hrs_per_day,
+                    active_hours_count
+                )
+            )
+        active_hours = set()
+        while len(active_hours) < hrs_per_day:
+            active_hours.add(random.randrange(hrs_of_day[0], hrs_of_day[1] + 1))
+        self.active_hours = active_hours
 
     def event_activate(self):
-        self.midnight = self.area.now.start_of("day").hour_(0)
-        self.period_active = self.calculate_active_period()
+        self.energy_per_slot = (
+            self.daily_energy_required
+            /
+            (self.hrs_per_day * Interval(hours=1) / self.area.config.slot_length)
+        )
 
     def event_tick(self, *, area):
-        energy_to_buy = 0
-        remaining_energy_required = self.remaining_energy_requirement()
-        # remaining_energy_required <= 50
-        if remaining_energy_required <= self.min_energy_buy:
-            energy_to_buy = remaining_energy_required
-        elif remaining_energy_required > self.min_energy_buy:
-            energy_to_buy = self.min_energy_buy
+        energy_to_buy = self.energy_per_slot
+        if self.random_factor:
+            energy_to_buy += energy_to_buy * random.random() * self.random_factor
+        if self.energy_missing:
+            energy_to_buy += self.energy_missing
+            self.energy_missing = 0
 
-        if energy_to_buy == 0.0:
+        energy_to_buy -= self.energy_bought_in_slot
+
+        if energy_to_buy < 0.0001:
             return
         markets = []
         for time, market in self.area.markets.items():
-            if time in self.period_active:
+            if time.hour in self.active_hours:
                 markets.append(market)
         if not markets:
             return
@@ -65,15 +82,16 @@ class FacebookDeviceStrategy(BaseStrategy):
             )
             if cheapest_offer:
                 self.accept_offer(market, cheapest_offer)
-                self.energy_consumed[pendulum.now()] = energy_to_buy
+                self.energy_bought_in_slot += energy_to_buy
         except MarketException:
             self.log.exception("An Error occurred while buying an offer")
 
     def event_market_cycle(self):
-        # Remove all dictionary entries of time, energy before midnight today
-        for time, energy_value in self.energy_consumed.items():
-            if time < self.midnight:
-                del self.energy_consumed[time]
+        self.energy_missing = 0
+        should_buy = list(self.area.past_markets.keys())[-1].hour in self.active_hours
+        if should_buy and self.energy_bought_in_slot < self.energy_per_slot:
+            self.energy_missing = self.energy_per_slot - self.energy_bought_in_slot
+        self.energy_bought_in_slot = 0
 
     # Returns daily energy required by the device in watt-hours at present
     def calculate_daily_energy_req(self):
@@ -81,30 +99,3 @@ class FacebookDeviceStrategy(BaseStrategy):
             return (self.consolidated_cycle / 365) * 1000
         elif self.hrs_per_day:
             return self.avg_power * self.hrs_per_day
-
-    # This function comes into picture if we decide to buy energy in percentages of the
-    # total energy required for the day
-    def calculate_percentage_of_energy_to_buy(self):
-        pass
-
-    # Returns the remaining energy required for the device on this day
-    def remaining_energy_requirement(self):
-        energy_consumed_today = 0  # energy consumed today or bought today
-        for time, energy_value in self.energy_consumed.items():
-            if time > self.midnight:
-                energy_consumed_today += energy_value
-
-        if energy_consumed_today >= self.daily_energy_required:
-            return 0
-        else:
-            return self.daily_energy_required - energy_consumed_today
-
-    def calculate_active_period(self):
-        if self.hrs_of_day[0] >= self.hrs_of_day[1]:
-            raise D3AException('Start hour cannot be greater than the end hour')
-        else:
-            # Instantiating a Period object for the hours the device is active
-            # https://pendulum.eustace.io/docs/#period
-            if self.midnight:
-                return self.midnight.end_of("hour").hour_(self.hrs_of_day[1]).diff(
-                    self.midnight.start_of("hour").hour_(self.hrs_of_day[0]))
