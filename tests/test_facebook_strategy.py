be2d74e188c1caf7cb0f0479a8282736b4013324
agatsoh <sohan.agate@gmail.com>
2017-11-28 16:06:18 +0530

    Fixing tests for the FB strategy
    (cherry picked from commit eef28b2e6baa4528d1869b88d109d943eb835aac)

--- tests/test_facebook_strategy.py
+++ tests/test_facebook_strategy.py
@@ -1,7 +1,6 @@
 import pytest
 import pendulum
 from pendulum import Pendulum
-from d3a.exceptions import D3AException
 from d3a.models.area import DEFAULT_CONFIG
 from d3a.models.market import Offer
 from d3a.models.strategy.facebook_device import FacebookDeviceStrategy
@@ -72,11 +71,14 @@ def market_test1():
 
 @pytest.fixture()
 def facebook_strategy_test1(area_test1, market_test1, called):
-    fb = FacebookDeviceStrategy(avg_power=620, hrs_per_day=4)
+    fb = FacebookDeviceStrategy(avg_power=620, hrs_per_day=4, hrs_of_day=(8, 12))
     fb.owner = area_test1
     fb.area = area_test1
-    fb.area.markets = {pendulum.now(): market_test1}
+    fb.area.markets = {TIME: market_test1}
     fb.accept_offer = called
+    if 10 not in fb.active_hours:
+        fb.active_hours.pop()
+        fb.active_hours.add(10)
     return fb
 
 
@@ -92,93 +94,25 @@ def test_device_accepts_offer(facebook_strategy_test1, market_test1):
     facebook_strategy_test1.event_tick(area=area_test1)
     assert facebook_strategy_test1.accept_offer.calls[0][0][1] == \
         repr(market_test1.sorted_offers[0])
-    for time, value in facebook_strategy_test1.energy_consumed.items():
-        assert value == 50
 
 
-@pytest.fixture()
-def area_test2():
-    return FakeArea(0)
+def test_event_narket_cycle(facebook_strategy_test1, market_test1):
+    facebook_strategy_test1.event_activate()
+    facebook_strategy_test1.event_tick(area=area_test1)
+    facebook_strategy_test1.area.past_markets = {TIME: market_test1}
+    facebook_strategy_test1.event_market_cycle()
+    assert facebook_strategy_test1.energy_missing == facebook_strategy_test1.energy_per_slot \
+        - market_test1.sorted_offers[0].energy*1000
 
 
-@pytest.fixture()
-def market_test2():
-    return FakeMarket(1)
+def test_device_adds_energy_missing(facebook_strategy_test1, market_test1):
+    facebook_strategy_test1.event_activate()
+    facebook_strategy_test1.event_tick(area=area_test1)
+    facebook_strategy_test1.area.past_markets = {TIME: market_test1}
+    facebook_strategy_test1.event_market_cycle()
+    facebook_strategy_test1.energy_missing == facebook_strategy_test1.energy_per_slot \
+        - market_test1.sorted_offers[0].energy*1000
 
-
-@pytest.fixture()
-def facebook_strategy_test2(area_test2, market_test2, called):
-    fb = FacebookDeviceStrategy(avg_power=5, hrs_per_day=0.33)
-    fb.owner = area_test2
-    fb.area = area_test2
-    fb.area.markets = {pendulum.now(): market_test2}
-    fb.accept_offer = called
-    return fb
-
-
-# Test if device does not buy energy after buying requirements are fulfilled
-def test_device_refrains_from_buying(area_test2, facebook_strategy_test2, market_test2):
-    facebook_strategy_test2.event_activate()
-    facebook_strategy_test2.event_tick(area=area_test2)
-    assert facebook_strategy_test2.accept_offer.calls[0][0][1] == \
-        repr(market_test2.sorted_offers[0])
-    facebook_strategy_test2.event_tick(area=area_test2)
-    # accept_offer does not get called hence it remains the same
-    assert facebook_strategy_test2.accept_offer.calls[0][0][1] == \
-        repr(market_test2.sorted_offers[0])
-
-
-@pytest.fixture()
-def area_test3():
-    return FakeArea(0)
-
-
-@pytest.fixture()
-def market_test3():
-    return FakeMarket(0)
-
-
-@pytest.fixture()
-def facebook_strategy_test3(area_test3, market_test3, called):
-    fb = FacebookDeviceStrategy(avg_power=1500, hrs_per_week=2.5)
-    fb.owner = area_test3
-    fb.area = area_test3
-    fb.area.markets = {pendulum.now(): market_test3}
-    fb.accept_offer = called
-    return fb
-
-
-def test_hrs_per_week(area_test3, facebook_strategy_test3):
-    assert facebook_strategy_test3.daily_energy_required == 1500*2.5/7
-
-
-def test_exception_wrong_hrsofday(area_test3, market_test3):
-    with pytest.raises(D3AException):
-        FacebookDeviceStrategy(avg_power=1500, hrs_per_week=2.5, hrs_of_day=(2, 1))
-
-
-@pytest.fixture()
-def area_test4():
-    return FakeArea(0)
-
-
-@pytest.fixture()
-def market_test4():
-    return FakeMarket(0)
-
-
-@pytest.fixture()
-def facebook_strategy_test4(area_test4, market_test4, called):
-    fb = FacebookDeviceStrategy(avg_power=620, hrs_per_day=4, hrs_of_day=(11, 20))
-    fb.owner = area_test4
-    fb.area = area_test4
-    fb.area.markets = {TIME: market_test4}
-    fb.accept_offer = called
-    return fb
-
-
-# Test if the device is inactive and does not accept offers outside the initialised hrs_of_day
-def test_device_inactive_outside_hrsofday(facebook_strategy_test4, area_test4, market_test4):
-    facebook_strategy_test4.event_activate()
-    facebook_strategy_test4.event_tick(area=area_test4)
-    assert len(facebook_strategy_test4.accept_offer.calls) == 0
+    facebook_strategy_test1.area.markets[TIME.add(hours=2)] = market_test1
+    facebook_strategy_test1.event_tick(area=area_test1)
+    assert facebook_strategy_test1.energy_missing == 0
