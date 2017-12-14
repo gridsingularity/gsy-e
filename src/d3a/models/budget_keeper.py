b491e451f247002a9dadb7acb8de39fd1b77ce7c
Jochen Müller <mail@jnmr.de>
2017-12-14 21:14:43 +0100

    Test BudgetKeeper in a setup, fix some mistakes

--- src/d3a/models/budget_keeper.py
+++ src/d3a/models/budget_keeper.py
@@ -4,6 +4,7 @@ from typing import Dict  # noqa
 from pendulum import Interval
 
 from d3a.models.area import Area
+from d3a.util import make_iaa_name
 
 
 class BudgetKeeper:
@@ -18,7 +19,10 @@ class BudgetKeeper:
     parameters = ('budget', 'days_per_period')
 
     def __init__(self, budget: float, days_per_period: int, area: Area = None):
-        self.area = area
+        if area is not None:
+            self.area = area
+        else:
+            self._area = None
         self.budget = budget
         self.days_per_period = days_per_period
         self.period_end = None
@@ -26,6 +30,15 @@ class BudgetKeeper:
         self.history = defaultdict(list)
         self.forecast = {}  # type: Dict[Area, float]
         self.enabled = set()
+
+    @property
+    def area(self):
+        return self._area
+
+    @area.setter
+    def area(self, new_area):
+        self._area = new_area
+        self._iaa = make_iaa_name(new_area)
 
     def set_priority(self, child, new_priority):
         if child in self.area.children:
@@ -35,7 +48,7 @@ class BudgetKeeper:
                                   .format(child))
 
     def activate(self):
-        assert self.area, "Area not set"
+        assert self.area is not None, "Area not set"
         self.begin_period()
 
     def process_market_cycle(self):
@@ -49,25 +62,28 @@ class BudgetKeeper:
             self.decide()
 
     def update_forecast(self):
+        consumed = defaultdict(float)
+        for trade in self._sold():
+            consumed[trade.buyer] += trade.offer.price
         for child in self.area.children:
-            consumed = defaultdict(float)
-            for trade in self._sold():
-                consumed[trade.offer.buyer] += trade.offer.price
-            for child in self.area.children:
-                self.history[child].append(consumed[child.name])
-                self.forecast[child] = sum(self.history[child]) / float(len(self.history[child]))
+            self.history[child].append(consumed[child.name])
+            self.forecast[child] = sum(self.history[child]) / float(len(self.history[child]))
 
     def compute_remaining(self):
         self.remaining -= sum(trade.offer.price for trade in self._sold())
 
     def _sold(self):
-        return (trade for trade in self.area.current_market.trades
-                if trade.offer.seller == self.area.name)
+        if self.area.current_market is None:
+            return []
+        else:
+            return (t for t in self.area.current_market.trades if t.seller == self._iaa)
 
     def decide(self):
         slot_cost_estimate = sum(self.forecast[child] for child in self.enabled)
         slots_left = (self.period_end - self.area.now) / self.area.config.slot_length
         acceptable = self.remaining / slots_left
+        self.area.log.error('Consumption forecast: {} expected, {} acceptable'
+                            .format(slot_cost_estimate, acceptable))
         if slot_cost_estimate > acceptable:
             for child in sorted(self.enabled,
                                 key=lambda c: (self.priority[c], -self.forecast[c])):
@@ -84,8 +100,9 @@ class BudgetKeeper:
 
     def _disable(self, child):
         self.enabled.remove(child)
-        child.strategy.fire_trigger("disable")
+        child._fire_trigger('disable')
+        self.area.log.error('BudgetKeeper disabled {}'.format(child.slug))
 
     def _enable(self, child):
         self.enabled.add(child)
-        child.strategy.fire_trigger("enable")
+        child._fire_trigger('enable')
