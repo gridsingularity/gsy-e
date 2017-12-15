26815b7f6a28c9ad3a4fb138c46a66e8ec7b3110
Jochen Müller <mail@jnmr.de>
2017-12-15 18:02:49 +0100

    test custom profile strategy in a setup

---
+++ src/d3a/setup/custom_profile_test.py
@@ -0,0 +1,36 @@
+from pendulum import Interval
+
+from d3a.models.appliance.custom_profile import CustomProfileAppliance
+from d3a.models.appliance.fridge import FridgeAppliance
+from d3a.models.appliance.pv import PVAppliance
+from d3a.models.area import Area
+from d3a.models.strategy.custom_profile import custom_profile_strategy_from_list
+from d3a.models.strategy.fridge import FridgeStrategy
+from d3a.models.strategy.pv import PVStrategy
+
+
+values = [0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+          0.0, 0.4, 0.9, 1.5, 1.7, 1.7,
+          0.1, 0.9, 1.1, 1.2, 0.5, 0.0,
+          0.0, 0.4, 0.0, 0.0, 0.0, 0.0]
+
+time_step = Interval(hours=1)
+
+
+def get_setup(config):
+    area = Area(
+        'Grid',
+        [
+            Area(
+                'House 1',
+                [
+                    Area('H1 Custom', appliance=CustomProfileAppliance(),
+                         strategy=custom_profile_strategy_from_list(values, time_step=time_step)),
+                    Area('H1 PV', strategy=PVStrategy(), appliance=PVAppliance()),
+                    Area('H1 Fridge', strategy=FridgeStrategy(), appliance=FridgeAppliance())
+                ]
+            )
+        ],
+        config=config
+    )
+    return area
