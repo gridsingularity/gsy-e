26787ff6ece3792b59936be97e0c917d3d4eac18
Ulrich Petri <ulo@ulo.pe>
2017-12-15 21:43:41 +0100

    Implement first version of daily device budget

--- src/d3a/models/strategy/facebook_device.py
+++ src/d3a/models/strategy/facebook_device.py
@@ -8,23 +8,26 @@ from d3a.models.strategy.base import BaseStrategy
 
 
 class FacebookDeviceStrategy(BaseStrategy):
-    def __init__(self, avg_power, hrs_per_day=None, hrs_of_day=(0, 23), consolidated_cycle=None,
-                 random_factor=0):
+    def __init__(self, avg_power, hrs_per_day, hrs_of_day=(0, 23), random_factor=0,
+                 daily_budget=None):
         super().__init__()
         self.avg_power = avg_power  # Average power in watts
-        if not hrs_per_day and not consolidated_cycle:
-            raise ValueError("Either 'hrs_per_day' or 'consolidated_cycle' is required")
         self.hrs_per_day = hrs_per_day  # Hrs the device is charged per day
         # consolidated_cycle is KWh energy consumed for the entire year
         self.daily_energy_required = self.avg_power * self.hrs_per_day
         # Random factor to modify buying
         self.random_factor = random_factor
+        # Budget for a single day in eur
+        self.daily_budget = daily_budget * 100
         # Energy consumed during the day ideally should not exceed daily_energy_required
-        self.energy_bought_in_slot = 0
-        self.energy_missing = 0
         self.energy_per_slot = None
-        # This is the minimum batch of energy that a device buys, please check whether this could
         self.energy_requirement = 0
+        # In ct. / kWh
+        self.max_acceptable_energy_price = 10**20
+        if self.daily_budget:
+            self.max_acceptable_energy_price = (
+                self.daily_budget / self.daily_energy_required * 1000
+            )
         # be a parameter on the constructor or if we want to deal in percentages
         self.hrs_of_day = hrs_of_day
         active_hours_count = (hrs_of_day[1] - hrs_of_day[0] + 1)
@@ -68,6 +71,7 @@ class FacebookDeviceStrategy(BaseStrategy):
                         for offer in market.sorted_offers
                         if (
                             offer.energy <= self.energy_requirement / 1000
+                            and offer.price / offer.energy <= self.max_acceptable_energy_price
                         )
                     ],
                     key=lambda o: o[0].price / o[0].energy
