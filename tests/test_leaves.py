19576d4f93f87346f025849358056f0e42bdece2
Jochen Müller <mail@jnmr.de>
2018-02-01 17:48:46 +0100

    more leaf classes

--- tests/test_leaves.py
+++ tests/test_leaves.py
@@ -1,6 +1,7 @@
 from d3a.models.appliance.fridge import FridgeAppliance
 from d3a.models.area import Area
-from d3a.models.leaves import Fridge
+from d3a.models.leaves import Fridge, CommercialProducer
+from d3a.models.strategy.commercial_producer import CommercialStrategy
 from d3a.models.strategy.fridge import FridgeStrategy, DEFAULT_RISK
 
 
@@ -14,3 +15,9 @@ def test_fridge_leaf():
 def test_fridge_leaf_default_risk():
     leaf = Fridge(name="fridge")
     assert leaf.strategy.risk == DEFAULT_RISK
+
+
+def test_commercial_producer_leaf():
+    leaf = CommercialProducer(name="cep", energy_range_wh=(10, 100), energy_price=23)
+    assert isinstance(leaf.strategy, CommercialStrategy)
+    assert leaf.strategy.energy_range_wh == (10, 100) and leaf.strategy.energy_price == 23
