f4d59540f7bd705e958d8170bd59c116a73d56fc
Oliver Bünting <oliver@brainbot.com>
2018-03-02 18:20:43 +0100

    Updated default.py according to Jira Ticket

--- src/d3a/setup/default.py
+++ src/d3a/setup/default.py
@@ -1,46 +1,38 @@
 from d3a.models.appliance.pv import PVAppliance
+from d3a.models.appliance.simple import SimpleAppliance
 from d3a.models.appliance.switchable import SwitchableAppliance
 from d3a.models.area import Area
+from d3a.models.strategy.commercial_producer import CommercialStrategy
 from d3a.models.strategy.load_hours_fb import LoadHoursStrategy
 from d3a.models.strategy.pv import PVStrategy
-from d3a.models.strategy.storage import StorageStrategy
 
 
 def get_setup(config):
     area = Area(
         'Grid',
         [
-                    Area(
-                        'House 1',
-                        [
-                            Area('H1 General Load',
-                                 strategy=LoadHoursStrategy(
-                                     avg_power=100,
-                                     hrs_per_day=4,
-                                     hrs_of_day=(12, 15)
-                                 ),
-                                 appliance=SwitchableAppliance()),
-                            Area('H1 Storage1',
-                                 strategy=StorageStrategy(initial_capacity=0.6),
-                                 appliance=SwitchableAppliance()
-                                 ),
-                        ]
-                    ),
-                    Area(
-                        'House 2',
-                        [
-                            Area('H2 General Load',
-                                 strategy=LoadHoursStrategy(
-                                     avg_power=100,
-                                     hrs_per_day=4,
-                                     hrs_of_day=(12, 15)
-                                 ),
-                                 appliance=SwitchableAppliance()),
-                            Area('H2 PV',
-                                 strategy=PVStrategy(6, 80),
-                                 appliance=PVAppliance()),
-                        ]
-                    ),
+            Area(
+                'House 2',
+                [
+                    Area('H2 General Load',
+                         strategy=LoadHoursStrategy(
+                             avg_power=100,
+                             hrs_per_day=4,
+                             hrs_of_day=(12, 15)
+                         ),
+                         appliance=SwitchableAppliance()),
+                    Area('H2 PV',
+                         strategy=PVStrategy(6, 80),
+                         appliance=PVAppliance()
+                         ),
+                ]
+            ),
+
+            Area('Commercial Energy Producer',
+                 strategy=CommercialStrategy(energy_range_wh=(40, 120), energy_price=30),
+                 appliance=SimpleAppliance()
+                 ),
+
         ],
         config=config
     )
