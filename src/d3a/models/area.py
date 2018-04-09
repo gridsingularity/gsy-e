e78e8c63e1fb5eb48c7065ab2ba29efd97917af8
Spyros Tzavikas <spyros@gridsingularity.com>
2018-04-09 17:39:43 +0200

    D3ASIM-278: Refactored method names

--- src/d3a/models/area.py
+++ src/d3a/models/area.py
@@ -32,7 +32,7 @@ DEFAULT_CONFIG = SimulationConfig(
 
 
 class Area:
-    _area_id = 1
+    _area_id_counter = 1
 
     def __init__(self, name: str = None, children: List["Area"] = None,
                  strategy: BaseStrategy = None,
@@ -44,8 +44,8 @@ class Area:
         self.current_tick = 0
         self.name = name
         self.slug = slugify(name, to_lower=True)
-        self.area_id = Area._area_id
-        Area._area_id += 1
+        self.area_id = Area._area_id_counter
+        Area._area_id_counter += 1
         self.parent = None
         self.children = children if children is not None else []
         for child in self.children:
