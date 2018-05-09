a79f1243f01092f7bec1b14b9592a876430f579c
Hannes Diedrich <hannes@gridsingularity.com>
2018-05-09 09:58:07 +0200

    d3asim-343: First integration tests.

--- features/integration_tests.feature
+++ features/integration_tests.feature
@@ -1,16 +1,20 @@
-Feature: Do integration test
+Feature: Run integration tests
 
-#  Scenario: Run an integration test
-#     Given we have a scenario named default_2b
-#     When we run the d3a simulation with it
-#      Then the results are tested
+  Scenario Outline: Run integration tests on console
+     Given we have a scenario named <scenario>
+     And d3a is installed
+     When we run the d3a simulation on console with <scenario>
+     Then we test the export functionality of <scenario>
+  Examples: Scenarios
+     | scenario |
+     | default_2a |
 
   Scenario Outline: Run integration tests
      Given we have a scenario named <scenario>
      And d3a is installed
-     When we run the d3a simulation with <scenario>
-     Then the results are tested for <scenario>
-  Examples: Scenarios
-    | scenario |
-    | default_2a |
-    | default_2b |
+     When we run the d3a simulation with <scenario> [<duration>, <slot_length>, <tick_length>]
+     Then we test the output of the simulation of <scenario> [<duration>, <slot_length>, <tick_length>]
+  Examples: Settings
+     | scenario   | duration | slot_length | tick_length |
+     | default_2a |    2     |      15     |      1      |
+     | default_2b |    4     |      10     |      5      |
--- features/steps/integration_tests.py
+++ features/steps/integration_tests.py
@@ -1,15 +1,18 @@
-from behave import given
-from behave import when
-from behave import then
 import os
 import importlib
 import glob
 import shutil
+from pendulum.interval import Interval
+from behave import given
+from behave import when
+from behave import then
+
+from d3a.models.config import SimulationConfig
+from d3a.simulation import Simulation
 
 
 @given('we have a scenario named {scenario}')
 def scenario_check(context, scenario):
-    assert importlib.util.find_spec("d3a")
     scenario_file = "./src/d3a/setup/{}.py".format(scenario)
     if not os.path.isfile(scenario_file):
         raise FileExistsError("File not found: {}".format(scenario_file))
@@ -20,27 +23,75 @@ def install_check(context):
     assert importlib.util.find_spec("d3a") is not None
 
 
-@when('we run the d3a simulation with {scenario}')
-def run_sim(context, scenario):
-
+@when('we run the d3a simulation on console with {scenario}')
+def run_sim_console(context, scenario):
     context.export_path = os.path.join("./d3a-simulation", "integration_tests", scenario)
     if os.path.isdir(context.export_path):
         shutil.rmtree(context.export_path)
     os.makedirs(context.export_path, exist_ok=True)
-    os.system("d3a -l ERROR run -d 2h --setup={scenario} --export --export-path={export_path} "
+    os.system("d3a -l FATAL run -d 2h --setup={scenario} --export --export-path={export_path} "
               "--exit-on-finish".format(export_path=context.export_path, scenario=scenario))
 
 
-@then('the results are tested for {scenario}')
-def test_data(context, scenario):
-
+@then('we test the export functionality of {scenario}')
+def test_export_data_csv(context, scenario):
     data_fn = "grid.csv"
     sim_data_csv = glob.glob(os.path.join(context.export_path, "*", data_fn))
-
     if len(sim_data_csv) != 1:
         print(os.path.join(context.export_path, "*", data_fn))
         raise FileExistsError("Not found in {path}: {file} ".format(path=context.export_path,
                                                                     file=data_fn))
     else:
-        print(sim_data_csv[0])
-    # TODO: check the csv data for valid data
+        shutil.rmtree(context.export_path)
+
+
+@when('we run the d3a simulation with {scenario} [{duration}, {slot_length}, {tick_length}]')
+def run_sim(context, scenario, duration, slot_length, tick_length):
+
+    simulation_config = SimulationConfig(Interval(hours=int(duration)),
+                                         Interval(minutes=int(slot_length)),
+                                         Interval(seconds=int(tick_length)), market_count=5)
+
+    slowdown = 0
+    seed = 0
+    paused = False
+    pause_after = Interval()
+    repl = False
+    export = False
+    export_path = None
+    reset_on_finish = False
+    reset_on_finish_wait = Interval()
+    exit_on_finish = True
+    exit_on_finish_wait = Interval()
+
+    api_url = "http://localhost:5000/api"
+    simulation = Simulation(
+        scenario,
+        simulation_config,
+        slowdown,
+        seed,
+        paused,
+        pause_after,
+        repl,
+        export,
+        export_path,
+        reset_on_finish,
+        reset_on_finish_wait,
+        exit_on_finish,
+        exit_on_finish_wait,
+        api_url
+    )
+    simulation.run()
+
+    context.simulation = simulation
+
+
+@then('we test the output of the simulation of '
+      '{scenario} [{duration}, {slot_length}, {tick_length}]')
+def test_output(context, scenario, duration, slot_length, tick_length):
+
+    # Check if simulation ran through
+    # (check if number of last slot is the maximal number of slots):
+    no_of_slots = (int(duration) * 60 / int(slot_length)) + 1
+    assert no_of_slots == context.simulation.area.current_slot
+    # TODO: Implement more sophisticated tests for success of simulation
