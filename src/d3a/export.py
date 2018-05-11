6f329f09ef3b14cb68b418d4ec9ca04984d6d601
faizan2590 <faizan2590@gmail.com>
2018-05-11 10:06:20 +0200

    Minor changes

--- src/d3a/export.py
+++ src/d3a/export.py
@@ -2,7 +2,6 @@ import csv
 import json
 import logging
 import pathlib
-from slugify import slugify
 
 import pandas as pd
 import os
@@ -17,7 +16,6 @@ from d3a.models.strategy.predef_load import DefinedLoadStrategy
 from d3a.models.strategy.pv import PVStrategy
 from d3a.models.strategy.storage import StorageStrategy
 
-slugify.to_lower = True
 
 _log = logging.getLogger(__name__)
 
@@ -185,8 +183,7 @@ def _export_area_energy(area, directory):
 
 def _export_iaa_energy(area, directory):
     try:
-        i = 1
-        for child in area.children:
+        for i, child in enumerate(area.children, 1):
             if child.children:
                 with open(_file_path(directory, "{}-residual-trades".format(child.slug)), 'w')\
                         as csv_file:
@@ -201,7 +198,6 @@ def _export_iaa_energy(area, directory):
                                 writer.writerow((slot, ) + (trade.offer.price, trade.offer.energy))
                             else:
                                 pass
-            i += 1
 
     except OSError:
         _log.exception("Could not export area trades")
