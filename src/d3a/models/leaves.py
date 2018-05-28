c076e32945b48910c80d02502303c08d08af9ff2
faizan2590 <faizan2590@gmail.com>
2018-05-28 15:19:52 +0200

    Minor changes

--- src/d3a/models/leaves.py
+++ src/d3a/models/leaves.py
@@ -10,7 +10,7 @@ from d3a.models.strategy.greedy_night_storage import NightStorageStrategy
 from d3a.models.strategy.pv import PVStrategy
 from d3a.models.strategy.storage import StorageStrategy
 from d3a.models.strategy.permanent import PermanentLoadStrategy
-from d3a.models.strategy.load_hours_fb import LoadHoursStrategy
+from d3a.models.strategy.load_hours_fb import LoadHoursStrategy, CellTowerLoadHoursStrategy
 
 
 class Leaf(Area):
@@ -70,4 +70,14 @@ class Storage(Leaf):
 
 class GeneralLoad(Leaf):
     strategy_type = LoadHoursStrategy
+    appliance_type = SwitchableAppliance
+
+
+class LoadHours(Leaf):
+    strategy_type = LoadHoursStrategy
+    appliance_type = SwitchableAppliance
+
+
+class CellTower(Leaf):
+    strategy_type = CellTowerLoadHoursStrategy
     appliance_type = SwitchableAppliance
