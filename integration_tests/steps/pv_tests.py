d76b58f8027a5e1b1fca0c164bc6dfb2b29c9bea
Spyros Tzavikas <spyros@gridsingularity.com>
2018-07-18 15:14:16 +0200

    D3ASIM-591: Added fixes from review comments.

--- integration_tests/steps/pv_tests.py
+++ integration_tests/steps/pv_tests.py
@@ -12,13 +12,13 @@ def storages_pv_min_selling_rate(context):
     for slot, market in house1.past_markets.items():
         for trade in market.trades:
             # Storage 2 should never buy due to break even point being lower than the
-            # PV min selling rater
+            # PV min selling rate
             assert trade.buyer != storage2.name
             if trade.buyer == storage1.name:
                 # Storage 1 should buy energy offers with rate more than the PV min sell rate
-                assert trade.offer.price / trade.offer.energy > pv.min_selling_price.m
+                assert trade.offer.price / trade.offer.energy >= pv.min_selling_price.m
 
     for slot, market in house2.past_markets.items():
         assert all(trade.seller == pv.name for trade in market.trades)
-        assert all(trade.offer.price / trade.offer.energy > pv.min_selling_price.m
+        assert all(trade.offer.price / trade.offer.energy >= pv.min_selling_price.m
                    for trade in market.trades)
