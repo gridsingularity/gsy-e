960ff0729b1b86d4c3743a3b519e294321eba57d
faizan2590 <faizan2590@gmail.com>
2018-07-31 10:03:25 +0200

    Refactoring storage_strategy_break_even_range setup file

--- src/d3a/setup/strategy_tests/storage_strategy_break_even_range.py
+++ src/d3a/setup/strategy_tests/storage_strategy_break_even_range.py
@@ -2,10 +2,15 @@ from d3a.models.appliance.switchable import SwitchableAppliance
 from d3a.models.area import Area
 from d3a.models.strategy.storage import StorageStrategy
 from d3a.models.strategy.commercial_producer import CommercialStrategy
-from d3a.models.strategy.load_hours_fb import LoadHoursStrategy, CellTowerLoadHoursStrategy
+from d3a.models.strategy.load_hours_fb import LoadHoursStrategy
 from d3a.models.appliance.simple import SimpleAppliance
 from d3a.models.appliance.pv import PVAppliance
 from d3a.models.strategy.pv import PVStrategy
+
+
+'''
+This setup file is being modified to mimic a typical modern household having a hybrid
+PV-ESS system.'''
 
 
 def get_setup(config):
@@ -16,40 +21,20 @@ def get_setup(config):
                 'House 1',
                 [
                     Area('H1 General Load', strategy=LoadHoursStrategy(avg_power_W=200,
-                                                                       hrs_per_day=6,
+                                                                       hrs_per_day=24,
                                                                        hrs_of_day=list(
-                                                                           range(12, 18)),
+                                                                           range(0, 24)),
                                                                        acceptable_energy_rate=35),
                          appliance=SwitchableAppliance()),
                     Area('H1 Storage1', strategy=StorageStrategy(risk=10, initial_capacity=0.6,
                                                                  break_even=(26.99, 27.01),
                                                                  max_abs_battery_power=5.0),
                          appliance=SwitchableAppliance()),
-                    Area('H1 Storage2', strategy=StorageStrategy(risk=10, initial_capacity=0.6,
-                                                                 break_even=(26.99, 27.01),
-                                                                 max_abs_battery_power=5.0),
-                         appliance=SwitchableAppliance()),
-                ]
-            ),
-            Area(
-                'House 2',
-                [
-                    Area('H2 General Load', strategy=LoadHoursStrategy(avg_power_W=200,
-                                                                       hrs_per_day=4,
-                                                                       hrs_of_day=list(
-                                                                           range(12, 16)),
-                                                                       acceptable_energy_rate=35),
-                         appliance=SwitchableAppliance()),
-                    Area('H2 PV', strategy=PVStrategy(4, 80),
+                    Area('H1 PV', strategy=PVStrategy(4, 80),
                          appliance=PVAppliance()),
 
                 ]
             ),
-            Area('Cell Tower', strategy=CellTowerLoadHoursStrategy(avg_power_W=100,
-                                                                   hrs_per_day=24,
-                                                                   hrs_of_day=list(range(0, 24)),
-                                                                   acceptable_energy_rate=35),
-                 appliance=SwitchableAppliance()),
             Area('Commercial Energy Producer',
                  strategy=CommercialStrategy(energy_rate=30),
                  appliance=SimpleAppliance()),
