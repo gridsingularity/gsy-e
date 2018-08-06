24bacede699ae14cd07ca64f8f3aa7df2d31a995
faizan2590 <faizan2590@gmail.com>
2018-08-06 17:58:10 +0200

    Adapting to PR comments

--- src/d3a/setup/strategy_tests/user_profile_load_csv.py
+++ src/d3a/setup/strategy_tests/user_profile_load_csv.py
@@ -8,7 +8,6 @@ from d3a.models.strategy.predefined_load import DefinedLoadStrategy
 from d3a.models.appliance.simple import SimpleAppliance
 from d3a.models.strategy.predefined_pv import d3a_path
 import os
-from d3a.models.config import SimulationConfig
 
 
 """
@@ -19,14 +18,7 @@ acceptable_energy_rate is optional.
 profile_path = os.path.join(d3a_path, "resources/LOAD_DATA_1.csv")
 
 
-market_maker_rate = {
-    2: 32, 3: 33, 4: 34, 5: 35, 6: 36, 7: 37, 8: 38,
-    9: 37, 10: 38, 11: 39, 14: 34, 15: 33, 16: 32,
-    17: 31, 18: 30, 19: 31, 20: 31, 21: 31, 22: 29}
-
-
 def get_setup(config):
-    SimulationConfig.read_market_maker_rate(config, market_maker_rate)
     area = Area(
         'Grid',
         [
