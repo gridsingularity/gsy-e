7da2e1e197961858d5bcc7dc845050c171f53770
Hannes Diedrich <hannes@gridsingularity.com>
2018-08-21 15:19:00 +0200

    Refactored docstrings of custom_pv.

--- src/d3a/models/strategy/custom_pv.py
+++ src/d3a/models/strategy/custom_pv.py
@@ -6,8 +6,7 @@ class CustomPvStrategy(PVStrategy):
     def produced_energy_forecast_kWh(self):
         """
         Overwrites d3a.models.strategy.pv.produced_energy_forecast_kWh
-        and returns the energy production of the custom PV for each market slot.
-        Function is called on every ACTIVATE event.
+        Is called on every ACTIVATE event.
         :return: dictionary that describes Energy production in kWh for each market slot:
                  self.energy_production_forecast_kWh
                  len(self.energy_production_forecast_kWh.keys) = slot_count
@@ -18,8 +17,8 @@ class CustomPvStrategy(PVStrategy):
     def calculate_initial_sell_rate(self, current_time_h):
         """
         Overrides d3a.models.strategy.update_frequency.calculate_initial_sell_rate
-        and returns the initial value of the sell energy rate for each hour of the simulation
-        Function is called on every MARKET_CYCLE event.
+        Is called on every MARKET_CYCLE event.
+        Returns the initial value of the sell energy rate for each hour of the simulation
         :param current_time_h: slot time in hours (e.g. market.time_slot.hour)
         :return: energy rate
                  e.g.: self.area.config.market_maker_rate[current_time_h]
@@ -30,9 +29,8 @@ class CustomPvStrategy(PVStrategy):
     def decrease_energy_price_over_ticks(self):
         """
         Overrides d3a.models.strategy.update_frequency.decrease_energy_price_over_ticks
-        and should be used to modify the price over the ticks for the selected market.
-        Function is called on every EVENT_TICK event.
-        :return: Nothing
+        Is called on every EVENT_TICK event.
+        Should be used to modify the price decrease over the ticks for the selected market.
         """
 
         pass
