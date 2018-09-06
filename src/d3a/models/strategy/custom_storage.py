435458a23d839116940bf72cd9482ce74fdaf028
Hannes Diedrich <hannes@gridsingularity.com>
2018-09-06 10:01:24 +0200

    D3ASIM-665: Added update_posted_bids to CustomStorageStrategy.

--- src/d3a/models/strategy/custom_storage.py
+++ src/d3a/models/strategy/custom_storage.py
@@ -51,3 +51,14 @@ class CustomStorageStrategy(StorageStrategy):
         """
 
         pass
+
+    def update_posted_bids(self, market):
+        """
+        Overwrites d3a.models.strategy.load_hours_fb.update_posted_bids
+        Is called on every TICK event.
+        Should be used to modify the price of the bids over the ticks for the selected market.
+        In the default implementation, an increase of the energy over the ticks is implemented
+        :param market: market object
+        """
+
+        pass
--- src/d3a/setup/jira/test_strategy_custom_storage.py
+++ src/d3a/setup/jira/test_strategy_custom_storage.py
@@ -1,4 +1,10 @@
 from d3a.models.strategy.storage import StorageStrategy
+from d3a.models.strategy.const import ConstSettings
+
+"""
+Example file for StorageStrategy.
+Is also used for integrationtest.
+"""
 
 
 class CustomStorageStrategy(StorageStrategy):
@@ -41,3 +47,28 @@ class CustomStorageStrategy(StorageStrategy):
         Sell on the most recent future market
         """
         return self.area.next_market
+
+    def update_posted_bids(self, market):
+        """
+        Copy of the original code
+        This is  not tested in the integrationtest (Scenario: "Custom storage works as expected")
+        Because this functionality is already tests in the StorageStrategy
+        """
+        # Decrease the selling price over the ticks in a slot
+        current_tick_number = self.area.current_tick % self.area.config.ticks_per_slot
+        elapsed_seconds = current_tick_number * self.area.config.tick_length.seconds
+        if (
+                # FIXME: Make sure that the offer reached every system participant.
+                # FIXME: Therefore it can only be update (depending on number of niveau and
+                # FIXME: InterAreaAgent min_offer_age
+                current_tick_number > ConstSettings.MAX_OFFER_TRAVERSAL_LENGTH
+                and elapsed_seconds > self._increase_rate_timepoint_s
+        ):
+            self._increase_rate_timepoint_s += self._increase_frequency_s
+            existing_bids = list(self.get_posted_bids(market))
+            for bid in existing_bids:
+                market.delete_bid(bid.id)
+                self.remove_bid_from_pending(bid.id, market)
+                self.post_bid(market,
+                              bid.energy * self._get_current_energy_rate(current_tick_number),
+                              bid.energy)
