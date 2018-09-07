6876f097d0648c0699be252f5f6b29b10e82f29e
Spyros Tzavikas <spyros@gridsingularity.com>
2018-09-07 15:50:16 +0200

    BC: Updated solc version on Travis.

--- .support/travis-install.sh
+++ .support/travis-install.sh
@@ -10,8 +10,8 @@ fi
 if [ "$TOXENV" == "py36,coverage" ] || [ "$TOXENV" == "py36" ]; then
 	# Arghhhh travis. Sudo is not available
 	mkdir $HOME/solc
-	wget https://launchpad.net/~ethereum/+archive/ubuntu/ethereum/+files/solc_0.4.17-0ubuntu1~trusty_amd64.deb
-	dpkg -x solc_0.4.17-0ubuntu1~trusty_amd64.deb $HOME/solc
+	wget https://launchpad.net/~ethereum/+archive/ubuntu/ethereum/+files/solc_0.4.24-0ubuntu1~trusty_amd64.deb
+	dpkg -x solc_0.4.24-0ubuntu1~trusty_amd64.deb $HOME/solc
 fi
 
 pip install -U tox
