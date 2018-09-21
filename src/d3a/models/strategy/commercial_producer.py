675b8aa72a60067bbf1d0e914cb5e1e8ca8059a5
Spyros Tzavikas <spyros@gridsingularity.com>
2018-09-21 10:45:33 +0200

    D3ASIM-674: Implemented balancing offer posting for commercial producer. Added unit tests.

--- src/d3a/models/strategy/commercial_producer.py
+++ src/d3a/models/strategy/commercial_producer.py
@@ -2,6 +2,7 @@ import sys
 from d3a.models.strategy import ureg, Q_
 
 from d3a.models.strategy.base import BaseStrategy
+from d3a.device_registry import DeviceRegistry
 
 
 class CommercialStrategy(BaseStrategy):
@@ -22,10 +23,32 @@ class CommercialStrategy(BaseStrategy):
         for market in self._markets_to_offer_on_activate():
             self.offer_energy(market)
 
+        for market in self.area.balancing_markets.values():
+            self.offer_balancing_energy(market)
+
+    def offer_balancing_energy(self, market):
+        if self.owner.name not in DeviceRegistry.REGISTRY:
+            return
+
+        # The second tuple member in the device registry is the balancing supply rate
+        # TODO: Consider adding infinite balancing demand offers in addition to supply, if we
+        # assume that CommercialProducer is a grid connection and not a power plant.
+        balancing_supply_rate = DeviceRegistry.REGISTRY[self.owner.name][1]
+
+        offer = market.balancing_offer(
+            self.energy_per_slot_kWh.m * balancing_supply_rate,
+            self.energy_per_slot_kWh.m,
+            self.owner.name
+        )
+        self.offers.post(offer, market)
+
     def event_market_cycle(self):
         # Post new offers
         market = list(self.area.markets.values())[-1]
         self.offer_energy(market)
+
+        balancing_market = list(self.area.balancing_markets.values())[-1]
+        self.offer_balancing_energy(balancing_market)
 
     def offer_energy(self, market):
         energy_rate = self.area.config.market_maker_rate[market.time_slot_str] \
