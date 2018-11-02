790b52ff2b2c98c58bd6de8efb57131147d65bd5
faizan2590 <faizan2590@gmail.com>
2018-11-02 17:21:42 +0100

    D3ASIM-750: Fixing bug of dispatching OFFER_CHANGED event twice for Blockhain

--- src/d3a/setup/tobalaba/one_producer_load.py
+++ src/d3a/setup/tobalaba/one_producer_load.py
@@ -2,7 +2,7 @@ from d3a.models.appliance.simple import SimpleAppliance
 from d3a.models.appliance.switchable import SwitchableAppliance
 from d3a.models.area import Area
 from d3a.models.strategy.finite_power_plant import FinitePowerPlant
-from d3a.models.strategy.permanent import PermanentLoadStrategy
+from d3a.models.strategy.load_hours_fb import LoadHoursStrategy
 from d3a.models.strategy.const import ConstSettings
 
 
@@ -11,7 +11,8 @@ def get_setup(config):
     area = Area(
         'Grid',
         [
-            Area('Cell Tower', strategy=PermanentLoadStrategy(),
+            Area('Cell Tower', strategy=LoadHoursStrategy(avg_power_W=25,
+                                                          hrs_per_day=24),
                  appliance=SwitchableAppliance()),
             Area('Commercial Energy Producer',
                  strategy=FinitePowerPlant(energy_rate=30,
