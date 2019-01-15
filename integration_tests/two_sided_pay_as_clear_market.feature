b0e09f5526c04f649292c2793a68e18e2669780d
faizan2590 <faizan2590@gmail.com>
2019-01-15 10:42:04 +0100

    D3ASIM-884-1: Adding integration for Cumulative Traded Offers equal to cumulative Traded Bids in 2sided PAC

--- integration_tests/two_sided_pay_as_clear_market.feature
+++ integration_tests/two_sided_pay_as_clear_market.feature
@@ -3,9 +3,17 @@ Feature: Two sided pay_as_clear market tests
   Scenario: All Offers & Bids are traded based on Market Clearing Rate
      Given we have a scenario named two_sided_pay_as_clear/default_2a
      And d3a is installed
-     When we run the simulation with setup file two_sided_pay_as_clear.default_2a and parameters [24, 15, 15, 0, 1]
+     When we run the simulation with setup file two_sided_pay_as_clear.default_2a and parameters [24, 15, 15, 1, 1]
      Then all trades are equal to market_clearing_rate
      And buyers and sellers are not same
+
+  Scenario: Cumulative Traded Offers equal to cumulative Traded Bids
+     Given we have a scenario named two_sided_pay_as_clear/test_clearing_energy
+     And d3a is installed
+     When we run the simulation with setup file two_sided_pay_as_clear.test_clearing_energy and parameters [24, 15, 15, 1, 1]
+     Then all trades are equal to market_clearing_rate
+     And buyers and sellers are not same
+     And cummulative traded offer energy equal to cumulative bid energy
 
   Scenario: All Offers & Bids are traded with finite energy
      Given we have a scenario named two_sided_pay_as_clear/default_2a
---
+++ src/d3a/setup/two_sided_pay_as_clear/test_clearing_energy.py
@@ -0,0 +1,71 @@
+"""
+Copyright 2018 Grid Singularity
+This file is part of D3A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from d3a.models.appliance.simple import SimpleAppliance
+from d3a.models.appliance.switchable import SwitchableAppliance
+from d3a.models.area import Area
+from d3a.models.strategy.commercial_producer import CommercialStrategy
+from d3a.models.strategy.load_hours import LoadHoursStrategy
+from d3a.models.appliance.pv import PVAppliance
+from d3a.models.strategy.pv import PVStrategy
+from d3a.models.strategy.storage import StorageStrategy
+from d3a.models.const import ConstSettings
+
+
+def get_setup(config):
+
+    ConstSettings.IAASettings.MARKET_TYPE = 3
+    ConstSettings.GeneralSettings.MARKET_CLEARING_FREQUENCY_PER_SLOT = 3
+    ConstSettings.LoadSettings.INITIAL_BUYING_RATE = 35
+    ConstSettings.LoadSettings.FINAL_BUYING_RATE = 35
+    ConstSettings.StorageSettings.MIN_BUYING_RATE = 24.99
+    ConstSettings.StorageSettings.BREAK_EVEN_BUY = 25
+    ConstSettings.StorageSettings.BREAK_EVEN_SELL = 25.01
+
+    area = Area(
+        'Grid',
+        [
+            Area(
+                'House 1',
+                [
+                    Area('H1 General Load', strategy=LoadHoursStrategy(avg_power_W=100,
+                                                                       hrs_per_day=24,
+                                                                       hrs_of_day=list(
+                                                                           range(24)),
+                                                                       final_buying_rate=35),
+                         appliance=SwitchableAppliance()),
+                    Area('H1 Storage1', strategy=StorageStrategy(
+                        initial_capacity_kWh=0.6,
+                        break_even=(ConstSettings.StorageSettings.BREAK_EVEN_BUY,
+                                    ConstSettings.StorageSettings.BREAK_EVEN_SELL)),
+                         appliance=SwitchableAppliance()),
+                    Area('H1 Storage2', strategy=StorageStrategy(
+                        initial_capacity_kWh=0.6,
+                        break_even=(ConstSettings.StorageSettings.BREAK_EVEN_BUY,
+                                    ConstSettings.StorageSettings.BREAK_EVEN_SELL)),
+                         appliance=SwitchableAppliance()),
+                    Area('H1 PV', strategy=PVStrategy(4, 0),
+                         appliance=PVAppliance()),
+                    Area('H1 CEP',
+                         strategy=CommercialStrategy(energy_rate=30),
+                         appliance=SimpleAppliance()),
+                ]
+            ),
+        ],
+        config=config
+    )
+    return area
