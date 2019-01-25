ffd7b2baebd124b4c7da1dd375d753e496219fcd
Spyros Tzavikas <spyros@gridsingularity.com>
2019-01-25 16:53:46 +0100

    Pricing scheme was not passed correctly through d3a-web.

--- src/d3a/d3a_core/d3a_jobs.py
+++ src/d3a/d3a_core/d3a_jobs.py
@@ -70,9 +70,8 @@ def start(scenario, settings):
         scenario_name = 'json_arg'
         config.area = scenario
 
-    kwargs = {"no_export": True}
-    run_simulation(pricing_scheme=0,
-                   setup_module_name=scenario_name,
+    kwargs = {"no_export": True, "pricing_scheme": 0}
+    run_simulation(setup_module_name=scenario_name,
                    simulation_config=config,
                    slowdown=settings.get('slowdown', 0),
                    redis_job_id=job.id,
