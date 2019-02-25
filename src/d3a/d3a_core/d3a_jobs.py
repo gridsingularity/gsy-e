00f45ea43179c5400418b627149764eeb97192e2
Hannes Diedrich <hannes@gridsingularity.com>
2019-02-25 09:55:44 +0100

    D3ASIM: Fixed bug.

--- src/d3a/d3a_core/d3a_jobs.py
+++ src/d3a/d3a_core/d3a_jobs.py
@@ -41,8 +41,8 @@ def start(scenario, settings):
 
     if settings is None:
         settings = {}
-
-    settings = {k: v for k, v in settings.items() if v is not None or v != "None"}
+    else:
+        settings = {k: v for k, v in settings.items() if v is not None and v != "None"}
 
     advanced_settings = settings.get('advanced_settings', None)
     if advanced_settings is not None:
