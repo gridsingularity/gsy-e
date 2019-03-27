290108d8cc4cc72eb54b8cb56c8642882b4bbc77
Spyros Tzavikas <spyros@gridsingularity.com>
2019-03-27 13:47:56 +0100

    Reduced job count on launcher.py. Added debug print for the interpreter name.

--- src/d3a/d3a_core/launcher.py
+++ src/d3a/d3a_core/launcher.py
@@ -37,11 +37,13 @@ REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost')
 class Launcher:
     def __init__(self,
                  queue=None,
-                 max_jobs=4,
+                 max_jobs=2,
                  max_delay_seconds=2):
         self.queue = queue or Queue('d3a', connection=StrictRedis.from_url(REDIS_URL))
         self.max_jobs = max_jobs
         self.max_delay = timedelta(seconds=max_delay_seconds)
+        print("HERE IS THE pypy interpreter.")
+        print(python_executable)
         self.command = [sys.executable, 'src/d3a/d3a_core/d3a_jobs.py']
         self.job_array = []
 
