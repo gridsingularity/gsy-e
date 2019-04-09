c77381e215f0004f987555d58a87e2e2b61930c9
faizan2590 <faizan2590@gmail.com>
2019-04-09 18:37:17 +0200

    D3ASIM-1034: Adapting to PR comments

--- src/d3a/d3a_core/launcher.py
+++ src/d3a/d3a_core/launcher.py
@@ -34,10 +34,10 @@ MAX_JOBS = os.environ.get('D3A_MAX_JOBS_PER_POD', 2)
 class Launcher:
     def __init__(self,
                  queue=None,
-                 max_jobs=2,
+                 max_jobs=None,
                  max_delay_seconds=2):
         self.queue = queue or Queue('d3a', connection=StrictRedis.from_url(REDIS_URL))
-        self.max_jobs = max_jobs or MAX_JOBS
+        self.max_jobs = max_jobs if max_jobs is not None else MAX_JOBS
         self.max_delay = timedelta(seconds=max_delay_seconds)
         python_executable = sys.executable \
             if platform.python_implementation() != "PyPy" \
