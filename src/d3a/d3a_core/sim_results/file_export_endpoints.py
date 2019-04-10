94965646d82b7ac58aaa3d0be734397ec3521345
Spyros Tzavikas <spyros@gridsingularity.com>
2019-04-10 18:06:16 +0200

    D3ASIM-1087: Adding elements to the energy trade profile only in case of nonzero values.

--- src/d3a/d3a_core/sim_results/file_export_endpoints.py
+++ src/d3a/d3a_core/sim_results/file_export_endpoints.py
@@ -92,17 +92,21 @@ class FileExportEndpoints:
                 if trade_seller.startswith("IAA "):
                     trade_seller = trade_seller[4:]
                 if trade_seller not in out_dict["sold_energy"]:
-                    out_dict["sold_energy"][trade_seller] = dict(
-                        (m.time_slot, 0) for m in area.past_markets)
-                out_dict["sold_energy"][trade_seller][market.time_slot] += trade.offer.energy
+                    out_dict["sold_energy"][trade_seller] = {}
+                if market.time_slot not in out_dict["sold_energy"][trade_seller]:
+                    out_dict["sold_energy"][trade_seller][market.time_slot] = trade.offer.energy
+                else:
+                    out_dict["sold_energy"][trade_seller][market.time_slot] += trade.offer.energy
 
                 trade_buyer = trade.buyer
                 if trade_buyer.startswith("IAA "):
                     trade_buyer = trade_buyer[4:]
                 if trade_buyer not in out_dict["bought_energy"]:
-                    out_dict["bought_energy"][trade_buyer] = dict(
-                        (m.time_slot, 0) for m in area.past_markets)
-                out_dict["bought_energy"][trade_buyer][market.time_slot] += trade.offer.energy
+                    out_dict["bought_energy"][trade_buyer] = {}
+                if market.time_slot not in out_dict["bought_energy"][trade_buyer]:
+                    out_dict["bought_energy"][trade_buyer][market.time_slot] = trade.offer.energy
+                else:
+                    out_dict["bought_energy"][trade_buyer][market.time_slot] += trade.offer.energy
 
         for ks in ("sold_energy", "bought_energy"):
             out_dict[ks + "_lists"] = dict((ki, {}) for ki in out_dict[ks].keys())
