0f3623eba081d5e74fcd1feeb430e68041d10e29
faizan2590 <faizan2590@gmail.com>
2019-05-22 15:30:16 +0200

    D3ASIM-1149: Adding integrationtests

--- integration_tests/steps/integration_tests.py
+++ integration_tests/steps/integration_tests.py
@@ -325,6 +325,16 @@ def past_markets_not_in_memory(context):
     ConstSettings.GeneralSettings.KEEP_PAST_MARKETS = False
 
 
+@when('the reported cumulative grid trades are saved')
+def save_reported_cumulative_grid_trade_profile(context):
+    context.cumulative_grid_trades = deepcopy(
+        context.simulation.endpoint_buffer.cumulative_grid_trades)
+    context.cumulative_grid_trades_redis = \
+        deepcopy(context.simulation.endpoint_buffer.cumulative_grid_trades_redis)
+    context.cumulative_grid_balancing_trades = deepcopy(
+        context.simulation.endpoint_buffer.cumulative_grid_balancing_trades)
+
+
 @then('we test the export functionality of {scenario}')
 def test_export_data_csv(context, scenario):
     data_fn = "grid.csv"
@@ -856,3 +866,18 @@ def identical_unmatched_loads(context):
     for _, v in unmatched_loads_redis.items():
         assert any(len(DeepDiff(v, old_area_results)) == 0
                    for _, old_area_results in context.unmatched_loads_redis.items())
+
+
+@then('the cumulative grid trades are identical no matter if the past markets are kept')
+def identical_energy_trade_profiles(context):
+    # cumulative_grid_trades = context.simulation.endpoint_buffer.cumulative_grid_trades
+
+    cumulative_grid_trades_redis = context.simulation.endpoint_buffer.cumulative_grid_trades_redis
+
+    assert len(DeepDiff(cumulative_grid_trades_redis, context.cumulative_grid_trades_redis)) == 0
+
+    # The 2 simulation runs do not assign the same uuids to the same areas
+    # therefore we have to search whether there are elements with the same values
+    # for _, v in energy_trade_profile_redis.items():
+    #     assert any(len(DeepDiff(v, old_area_results)) == 0
+    #                for _, old_area_results in context.energy_trade_profile_redis.items())
