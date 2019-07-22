b34fe158a3f7d565534c8eabab14c0be23b72d34
Hannes Diedrich <hannes@gridsingularity.com>
2019-07-22 15:56:17 +0200

    D3ASIM-1338: Minor refactoring.

--- fabfile.py
+++ fabfile.py
@@ -149,7 +149,6 @@ def sync():
         local('pip install --no-deps -e .')
         # TODO: Workaround for https://github.com/ethereum/py-solc/issues/64
         local('pip install git+https://github.com/Jonasmpi/py-solc.git')
-
     _post_check()
 
 
