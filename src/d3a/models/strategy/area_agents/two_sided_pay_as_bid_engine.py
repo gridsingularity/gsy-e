2cd241e4883bb2db635bb280919f1545fa04a0dd
spyrostz <spyros@gridsingularity.com>
2019-09-13 10:35:45 +0200

    D3ASIM-1391: Satisfied flake8 for unused import statements.

--- src/d3a/models/strategy/area_agents/two_sided_pay_as_bid_engine.py
+++ src/d3a/models/strategy/area_agents/two_sided_pay_as_bid_engine.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from collections import namedtuple
-from typing import Dict
+from typing import Dict  # NOQA
 from d3a.models.strategy.area_agents.inter_area_agent import InterAreaAgent  # NOQA
 from d3a.models.strategy.area_agents.one_sided_engine import IAAEngine
 from d3a.d3a_core.exceptions import BidNotFound, MarketException
