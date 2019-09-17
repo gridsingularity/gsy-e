6f4458b0b35ee2f939c8fea343da5e41c34c59a6
Spyros Tzavikas <spyros@gridsingularity.com>
2019-09-17 11:05:56 +0200

    D3ASIM-1378: Added fixes from review comments

--- src/d3a/d3a_core/d3a_jobs.py
+++ src/d3a/d3a_core/d3a_jobs.py
@@ -72,7 +72,8 @@ def start(scenario, settings, events):
             "cloud_coverage": settings.get('cloud_coverage', GlobalConfig.cloud_coverage),
             "pv_user_profile": settings.get('pv_user_profile', None),
             "iaa_fee": settings.get('iaa_fee', GlobalConfig.iaa_fee),
-            "max_panel_power_W": settings.get('max_panel_power_W', GlobalConfig.iaa_fee)
+            "max_panel_power_W": settings.get('max_panel_power_W',
+                                              ConstSettings.PVSettings.MAX_PANEL_OUTPUT_W)
         }
 
         validate_global_settings(config_settings)
