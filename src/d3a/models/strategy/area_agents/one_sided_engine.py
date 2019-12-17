09e3c1c2a399c4ea6c9e079c919df61ee2e9e4ee
faizan2590 <faizan2590@gmail.com>
2019-12-17 15:04:09 +0100

    D3ASIM-1806: Enable offer with zero price to be forwarded.

--- src/d3a/models/strategy/area_agents/one_sided_engine.py
+++ src/d3a/models/strategy/area_agents/one_sided_engine.py
@@ -67,7 +67,9 @@ class IAAEngine:
             return self.markets.target.offer(**kwargs)
 
     def _forward_offer(self, offer, offer_id):
-        if offer.price == 0:
+        # TODO: This is an ugly solution. After the december release this check needs to
+        #  implemented after grid fee being incorporated while forwarding in target market
+        if offer.price < 0.0:
             self.owner.log.debug("Offer is not forwarded because price=0")
             return
 
