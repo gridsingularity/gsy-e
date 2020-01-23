437621167ff8e1fca70080491fc86a3e887a813a
Hannes Diedrich <hannes@gridsingularity.com>
2020-01-23 09:52:13 +0100

    D3ASIM-1865: Fixed integrationtests.

--- integration_tests/steps/integration_tests.py
+++ integration_tests/steps/integration_tests.py
@@ -455,7 +455,8 @@ def test_aggregated_result_files(context):
                  os.path.join(base_path, 'status.json'),
                  os.path.join(base_path, 'trade-detail.json'),
                  os.path.join(base_path, 'tree_summary.json'),
-                 os.path.join(base_path, 'unmatched_loads.json')]
+                 os.path.join(base_path, 'unmatched_loads.json'),
+                 os.path.join(base_path, 'kpi.json')]
 
     assert all(len(glob.glob(f)) == 1 for f in file_list)
     assert all(len(open(glob.glob(f)[0]).readlines()) > 0 for f in file_list)
