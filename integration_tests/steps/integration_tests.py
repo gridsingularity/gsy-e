e4cbfb1d1f7713d641fb7f524405116513112fd7
Hannes Diedrich <hannes@gridsingularity.com>
2020-01-23 11:11:00 +0100

    D3ASIM-1865: Added print for debugging integrationtest.

--- integration_tests/steps/integration_tests.py
+++ integration_tests/steps/integration_tests.py
@@ -458,6 +458,7 @@ def test_aggregated_result_files(context):
                  os.path.join(base_path, 'unmatched_loads.json'),
                  os.path.join(base_path, 'kpi.json')]
 
+    print(file_list)
     assert all(len(glob.glob(f)) == 1 for f in file_list)
     assert all(len(open(glob.glob(f)[0]).readlines()) > 0 for f in file_list)
 
