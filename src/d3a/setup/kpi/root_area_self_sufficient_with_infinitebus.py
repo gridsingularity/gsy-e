1ffea1cde46e81f36d6b9d13d1de58cd1230a508
Spyros Tzavikas <spyros@gridsingularity.com>
2020-01-24 15:00:01 +0100

    D3ASIM-1867: Added license header on top of the file.

--- src/d3a/setup/kpi/root_area_self_sufficient_with_infinitebus.py
+++ src/d3a/setup/kpi/root_area_self_sufficient_with_infinitebus.py
@@ -1,7 +1,19 @@
 """
-Setup for a microgrid + DSO
+Copyright 2018 Grid Singularity
+This file is part of D3A.
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from d3a.models.appliance.switchable import SwitchableAppliance
 from d3a.models.area import Area
@@ -11,6 +23,8 @@ from d3a.models.strategy.pv import PVStrategy
 from d3a_interface.constants_limits import ConstSettings
 from d3a.models.strategy.infinite_bus import InfiniteBusStrategy
 from d3a.models.appliance.simple import SimpleAppliance
+
+# Setup for a microgrid + DSO
 
 
 def get_setup(config):
