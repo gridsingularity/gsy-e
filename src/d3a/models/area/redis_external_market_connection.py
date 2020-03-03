1a2167d82b5093de1457b4c063e9978c7e2bca11
Spyros Tzavikas <spyros@gridsingularity.com>
2020-03-03 17:47:09 +0100

    D3ASIM-2068: Fixed channel names for redis connection for market stats, to conform to the format of the device channel names. Added new channel for adapting the market grid fee.

--- src/d3a/models/area/redis_external_market_connection.py
+++ src/d3a/models/area/redis_external_market_connection.py
@@ -1,5 +1,6 @@
 from redis import StrictRedis
 import json
+import d3a
 from d3a.d3a_core.redis_connections.redis_communication import REDIS_URL
 
 
@@ -14,17 +15,41 @@ class RedisMarketExternalConnection:
         self.redis_db.publish(channel, data)
 
     @property
+    def channel_prefix(self):
+        if d3a.constants.EXTERNAL_CONNECTION_WEB:
+            return f"external/{d3a.constants.COLLABORATION_ID}/{self.area.uuid}"
+        else:
+            return f"{self.area.slug}"
+
+    @property
     def _market_stats_channel(self):
-        return f"market_stats/{self.area.slug}"
+        return f"{self.channel_prefix}/market_stats"
+
+    @property
+    def _grid_fees_channel(self):
+        return f"{self.channel_prefix}/grid_fees"
 
     def sub_to_area_event(self):
-        self.pubsub.subscribe(**{self._market_stats_channel: self.market_stats_callback})
+        self.pubsub.subscribe(**{
+            f"{self.channel_prefix}/market_stats": self.market_stats_callback,
+            f"{self.channel_prefix}/grid_fees": self.grid_fees_callback
+        })
         self.pubsub.run_in_thread(daemon=True)
 
     def market_stats_callback(self, payload):
-        market_stats_response_channel = f"{self._market_stats_channel}/response"
+        market_stats_response_channel = f"{self.channel_prefix}/response/market_stats"
         payload_data = json.loads(payload["data"])
         ret_val = {"status": "ready",
+                   "command": "market_stats",
                    "market_stats":
                        self.area.stats.get_market_stats(payload_data["market_slots"])}
         self.publish(market_stats_response_channel, json.dumps(ret_val))
+
+    def grid_fees_callback(self, payload):
+        market_stats_response_channel = f"{self.channel_prefix}/response/grid_fees"
+        payload_data = json.loads(payload["data"])
+        self.area.transfer_fee_const = payload_data["fee"]
+        self.publish(market_stats_response_channel, json.dumps({
+            "status": "ready", "command": "grid_fees",
+            "fee": str(self.area.transfer_fee_const)})
+         )
