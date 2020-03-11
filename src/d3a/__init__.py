c0b5d0e1d4b818a42a90368072388e48ac425911
Spyros Tzavikas <spyros@gridsingularity.com>
2020-03-11 16:38:32 +0100

    D3ASIM-2099: Left the version file only on the setup.py file.

--- src/d3a/__init__.py
+++ src/d3a/__init__.py
@@ -2,9 +2,6 @@ import os
 
 from d3a.constants import DEFAULT_PRECISION
 
-# *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-VERSION = "0.3.0"
-
 
 def get_project_root():
     return os.path.dirname(__file__)
