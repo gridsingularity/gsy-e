79b6e8723950cd38fc7dcb1ceaf943a7acf43f8b
Spyros Tzavikas <spyros@gridsingularity.com>
2020-03-26 09:59:19 +0100

    D3ASIM-1967: Reverted setup file config to default values to avoid faulty integration test faiilure.

--- src/d3a/setup/jira/d3asim_1896.py
+++ src/d3a/setup/jira/d3asim_1896.py
@@ -19,15 +19,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from d3a.models.appliance.switchable import SwitchableAppliance
 from d3a.models.area import Area
 from d3a.models.strategy.storage import StorageStrategy
-from d3a_interface.constants_limits import ConstSettings
+from d3a_interface.constants_limits import ConstSettings, RateRange
 from d3a.models.strategy.infinite_bus import InfiniteBusStrategy
 from d3a.models.appliance.simple import SimpleAppliance
 
 
 def get_setup(config):
+    import d3a.constants
+    d3a.constants.DISPATCH_EVENTS_BOTTOM_TO_TOP = True
     ConstSettings.IAASettings.MARKET_TYPE = 2
     ConstSettings.GeneralSettings.DEFAULT_UPDATE_INTERVAL = 1
+    ConstSettings.GeneralSettings.MIN_UPDATE_INTERVAL = 1
     ConstSettings.GeneralSettings.DEFAULT_MARKET_MAKER_RATE = 30
+    ConstSettings.StorageSettings.SELLING_RATE_RANGE = RateRange(30, 15.1)
+    ConstSettings.StorageSettings.BUYING_RATE_RANGE = RateRange(5, 15)
 
     area = Area(
         'Grid',
@@ -45,7 +50,8 @@ def get_setup(config):
                                 initial_buying_rate=5,
                                 final_buying_rate=15,
                                 initial_selling_rate=30,
-                                final_selling_rate=15.1
+                                final_selling_rate=15.1,
+                                update_interval=1
                             ), appliance=SwitchableAppliance())
 
                         ], grid_fee_percentage=0, transfer_fee_const=0,
