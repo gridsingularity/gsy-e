615b64f7fd7964a7f41fc0740ef729a175e85d49
spyrostz <spyros@gridsingularity.com>
2020-04-17 17:33:43 +0200

    D3ASIM-2262: Fixed setup file that will be used for the respective integration test.

--- src/d3a/setup/kpi/d3asim_2262.py
+++ src/d3a/setup/kpi/d3asim_2262.py
@@ -1,12 +1,20 @@
+"""
+Setup for a microgrid + DSO
+
+
+"""
 import os
 
 from d3a.models.appliance.pv import PVAppliance
 from d3a.models.appliance.simple import SimpleAppliance
 from d3a.models.appliance.switchable import SwitchableAppliance
 from d3a.models.area import Area
+from d3a.models.strategy.market_maker_strategy import MarketMakerStrategy
 from d3a.models.strategy.predefined_load import DefinedLoadStrategy
 from d3a_interface.constants_limits import ConstSettings
 from d3a.models.strategy.infinite_bus import InfiniteBusStrategy
+from d3a.models.strategy.storage import StorageStrategy
+from d3a.models.strategy.load_hours import LoadHoursStrategy
 from d3a.models.strategy.predefined_pv import PVUserProfileStrategy
 
 current_dir = os.path.dirname(__file__)
@@ -15,7 +23,9 @@ current_dir = os.path.dirname(__file__)
 def get_setup(config):
 
     ConstSettings.GeneralSettings.DEFAULT_UPDATE_INTERVAL = 1
-    ConstSettings.IAASettings.MARKET_TYPE = 1
+    ConstSettings.IAASettings.MARKET_TYPE = 2
+    ConstSettings.GeneralSettings.KEEP_PAST_MARKETS = False
+    ConstSettings.IAASettings.MIN_OFFER_AGE = 1
 
     area = Area(
         'Grid',
@@ -26,38 +36,55 @@ def get_setup(config):
                     Area(
                         'House 1',
                         [
-                            Area('H1 General Load',
-                                 strategy=DefinedLoadStrategy(
-                                     daily_load_profile=os.path.join(current_dir,
-                                                                     "../../resources/KPI_L1.csv"),
-                                     initial_buying_rate=20,
-                                     final_buying_rate=30,
-                                     fit_to_limit=True),
+                            Area('H1 General Load', strategy=DefinedLoadStrategy(
+                                daily_load_profile=os.path.join(
+                                    current_dir, "../../resources/KPI_L1.csv"),
+                                initial_buying_rate=20,
+                                final_buying_rate=30,
+                                fit_to_limit=True),
                                  appliance=SwitchableAppliance()),
                             Area('H1 PV', strategy=PVUserProfileStrategy(
-                                power_profile=os.path.join(current_dir,
-                                                           "../../resources/KPI_PV1.csv"),
+                                power_profile=os.path.join(
+                                    current_dir, "../../resources/KPI_PV1.csv"),
                                 initial_selling_rate=20,
-                                final_selling_rate=20,
+                                final_selling_rate=15,
                                 fit_to_limit=True),
                                  appliance=PVAppliance()),
+                            Area('H1 General Load evening',
+                                 strategy=LoadHoursStrategy(
+                                     hrs_per_day=1,
+                                     hrs_of_day=list([22, 23]),
+                                     avg_power_W=2000,
+                                     initial_buying_rate=20,
+                                     final_buying_rate=30),
+                                 appliance=SwitchableAppliance()),
                         ], grid_fee_percentage=0, transfer_fee_const=0,
                     ),
+                    Area('central storage', strategy=StorageStrategy(
+                        initial_soc=10,
+                        min_allowed_soc=10,
+                        battery_capacity_kWh=6,
+                        max_abs_battery_power_kW=5,
+                        initial_buying_rate=12,
+                        final_buying_rate=20,
+                        initial_selling_rate=29,
+                        final_selling_rate=20.01),
+                         appliance=SwitchableAppliance()),
                     Area(
                         'House 2',
                         [
                             Area('H2 General Load', strategy=DefinedLoadStrategy(
-                                daily_load_profile=os.path.join(current_dir,
-                                                                "../../resources/KPI_L1.csv"),
+                                daily_load_profile=os.path.join(
+                                    current_dir, "../../resources/KPI_L1.csv"),
                                 initial_buying_rate=20,
                                 final_buying_rate=30,
                                 fit_to_limit=True),
                                  appliance=SwitchableAppliance()),
                             Area('H2 PV', strategy=PVUserProfileStrategy(
-                                power_profile=os.path.join(current_dir,
-                                                           "../../resources/KPI_PV2.csv"),
+                                power_profile=os.path.join(
+                                    current_dir, "../../resources/KPI_PV2.csv"),
                                 initial_selling_rate=20,
-                                final_selling_rate=20,
+                                final_selling_rate=15,
                                 fit_to_limit=True),
                                  appliance=PVAppliance()),
                         ], grid_fee_percentage=0, transfer_fee_const=0,
@@ -66,17 +93,17 @@ def get_setup(config):
                         'House 3',
                         [
                             Area('H3 General Load', strategy=DefinedLoadStrategy(
-                                daily_load_profile=os.path.join(current_dir,
-                                                                "../../resources/KPI_L1.csv"),
+                                daily_load_profile=os.path.join(
+                                    current_dir, "../../resources/KPI_L1.csv"),
                                 initial_buying_rate=20,
                                 final_buying_rate=30,
                                 fit_to_limit=True),
                                  appliance=SwitchableAppliance()),
                             Area('H3 PV', strategy=PVUserProfileStrategy(
-                                power_profile=os.path.join(current_dir,
-                                                           "../../resources/KPI_PV3.csv"),
+                                power_profile=os.path.join(
+                                    current_dir, "../../resources/KPI_PV3.csv"),
                                 initial_selling_rate=20,
-                                final_selling_rate=20,
+                                final_selling_rate=15,
                                 fit_to_limit=True),
                                  appliance=PVAppliance()),
                         ], grid_fee_percentage=0, transfer_fee_const=0,
@@ -84,11 +111,16 @@ def get_setup(config):
 
 
 
-                ], grid_fee_percentage=1, transfer_fee_const=0,
+                ], grid_fee_percentage=0, transfer_fee_const=0,
             ),
-            Area('DSO', strategy=InfiniteBusStrategy(energy_buy_rate=20, energy_sell_rate=30),
+            Area('Feed In Tariff', strategy=InfiniteBusStrategy(
+                energy_buy_rate=15.2,
+                energy_sell_rate=200),
                  appliance=SimpleAppliance()),
 
+            Area('Market Maker',
+                 strategy=MarketMakerStrategy(energy_rate=30, grid_connected=True)
+                 )
         ],
         config=config, grid_fee_percentage=0, transfer_fee_const=0,
     )
