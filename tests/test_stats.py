2262fb8a117a68fd9731bba28304a84713308ceb
Hannes Diedrich <hannes@gridsingularity.com>
2020-04-29 12:35:14 +0200

    D3ASIM-2303: Minor refactoring.

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -242,5 +242,5 @@ def test_energy_bills_report_correctly_market_fees(grid_fees):
     assert result["street"]["house1"]["market_fee"] == 0.04
     assert result["street"]["house2"]["market_fee"] == 0.01
     assert result["street"]['Accumulated Trades']["market_fee"] == 0.05
-    assert result["house1"]['External Trades']["market_fee"] == 0.00
-    assert result["house2"]['External Trades']["market_fee"] == 0.00
+    assert result["house1"]['External Trades']["market_fee"] == 0.0
+    assert result["house2"]['External Trades']["market_fee"] == 0.0
