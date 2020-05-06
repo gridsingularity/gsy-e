c50a592b0914eab9b26089d0f342c44e4cd2c32f
faizan2590 <faizan2590@gmail.com>
2020-05-06 03:38:27 +0200

    D3ASIM-2325: Enhancing start, pause, resume, rerun, abort support of siulation via UI.

--- src/d3a/d3a_core/simulation.py
+++ src/d3a/d3a_core/simulation.py
@@ -305,12 +305,16 @@ class Simulation:
             log.debug(f"Used {mbs_used} MBs.")
 
             for tick_no in range(tick_resume, config.ticks_per_slot):
+                tick_start = time.monotonic()
+                while self.paused:
+                    if time.monotonic() - tick_start > 600:
+                        self.is_stopped = True
+                        self.paused = False
                 # reset tick_resume after possible resume
                 tick_resume = 0
                 if console is not None:
                     self._handle_input(console)
                     self.paused_time += self._handle_paused(console)
-                tick_start = time.monotonic()
                 log.trace(
                     "Tick %d of %d in slot %d (%2.0f%%)",
                     tick_no + 1,
