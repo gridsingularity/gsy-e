925961a7076ee95f8ab6f152075d4c6da4f3577c
psurender <surender@gridsingularity.com>
2020-07-28 12:03:03 +0200

    D3ASIM2634 - updated comment for CLI Slowdown flaot value
    D3ASIM2634- updated comment for CLI Slowdown flaot value

--- src/d3a/d3a_core/cli.py
+++ src/d3a/d3a_core/cli.py
@@ -83,7 +83,7 @@ _setup_modules = available_simulation_scenarios
 @click.option('--slowdown', type=float, default=0,
               help="Slowdown factor [0 - 10,000]. "
                    "Where 0 means: no slowdown, ticks are simulated as fast as possible;"
-                   "You can give float value also which makes simulation slower"
+                   "You can give also float value to precisely control the simulation speed"
                    "and 100: ticks are simulated in realtime")
 @click.option('--seed', help="Manually specify random seed")
 @click.option('--paused', is_flag=True, default=False, show_default=True,
