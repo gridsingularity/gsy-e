05e5b4b6de2eac5a2daaaa51160241ffbd1fb399
psurender <surender@gridsingularity.com>
2020-07-30 18:16:49 +0200

    testing2

--- src/d3a/setup/default_2.py
+++ src/d3a/setup/default_2.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 # testing
-
+# testing3
 from d3a.models.appliance.pv import PVAppliance
 from d3a.models.appliance.simple import SimpleAppliance
 from d3a.models.appliance.switchable import SwitchableAppliance
