cbc01dd11356c8bf9b0927215a3f75127b4b3ce4
faizan2590 <faizan2590@gmail.com>
2020-09-30 13:34:37 +0200

    D3ASIM-2768: Adapting to PR comments.

--- src/d3a/d3a_core/sim_results/market_price_energy_day.py
+++ src/d3a/d3a_core/sim_results/market_price_energy_day.py
@@ -38,15 +38,15 @@ class MarketPriceEnergyDay:
         ]
         price_lists[area_dict['uuid']][current_market_slot].extend(trade_rates)
 
-    def update(self, area_result_dict={}, core_stats={}, current_market_slot=None):
-        if current_market_slot is None:
+    def update(self, area_result_dict=None, core_stats=None, current_market_slot=None):
+        if current_market_slot is None or area_result_dict is None or core_stats is None:
             return
         current_price_lists = self.gather_trade_rates(
             area_result_dict, core_stats, current_market_slot, {}
         )
         price_energy_redis_output = {}
         self._convert_output_format(current_price_lists, price_energy_redis_output,
-                                    core_stats, current_market_slot)
+                                    core_stats)
         if self.should_export_plots:
             self.calculate_csv_output(area_result_dict, price_energy_redis_output)
         else:
@@ -59,25 +59,21 @@ class MarketPriceEnergyDay:
             self.csv_output[area_dict['name']] = {
                 "price-currency": "Euros",
                 "load-unit": "kWh",
-                "price-energy-day": [],
-                "grid-fee-rate": []
+                "price-energy-day": []
             }
         self.csv_output[area_dict['name']]["price-energy-day"].\
             append(price_energy_redis_output[area_dict['uuid']]['price-energy-day'])
-        self.csv_output[area_dict['name']]["grid-fee-rate"].\
-            append(price_energy_redis_output[area_dict['uuid']]['grid-fee-rate'])
         for child in area_dict['children']:
             self.calculate_csv_output(child, price_energy_redis_output)
 
     @staticmethod
-    def _convert_output_format(price_energy, redis_output, core_stats, current_market_slot):
+    def _convert_output_format(price_energy, redis_output, core_stats):
         for node_uuid, trade_rates in price_energy.items():
             if node_uuid not in redis_output:
                 redis_output[node_uuid] = {
                     "price-currency": "Euros",
                     "load-unit": "kWh",
-                    "price-energy-day": [],
-                    "grid-fee-rate": []
+                    "price-energy-day": []
                 }
             redis_output[node_uuid]["price-energy-day"] = [
                 {
@@ -91,6 +87,5 @@ class MarketPriceEnergyDay:
             area_core_stats = core_stats.get(node_uuid, {})
             fee = area_core_stats['grid_fee_constant'] / 100 \
                 if key_in_dict_and_not_none(area_core_stats, 'grid_fee_constant') else 0.
-            fee_dict = {"time": current_market_slot, "value": fee}
 
-            redis_output[node_uuid]["grid-fee-rate"].append(fee_dict)
+            redis_output[node_uuid]["price-energy-day"][0].update({"fee_price": fee})
