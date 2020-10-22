3bce1b35e72d83abc643da8fda397c126105c44d
faizan2590 <faizan2590@gmail.com>
2020-10-22 12:42:01 +0200

    D3ASIM-2946: Adding uniform area types for UI.

--- src/d3a/d3a_core/sim_results/__init__.py
+++ src/d3a/d3a_core/sim_results/__init__.py
@@ -10,9 +10,8 @@ def is_load_node_type(area):
                             "LoadHoursExternalStrategy", "LoadProfileExternalStrategy"]
 
 
-def is_producer_node_type(area):
-    return area['type'] in ["CommercialStrategy", "FinitePowerPlant", "MarketMakerStrategy"] or \
-           is_pv_node_type(area)
+def is_bulk_power_producer(area):
+    return area['type'] in ["CommercialStrategy", "MarketMakerStrategy"]
 
 
 def is_pv_node_type(area):
@@ -21,12 +20,34 @@ def is_pv_node_type(area):
                             "PVPredefinedExternalStrategy"]
 
 
+def is_producer_node_type(area):
+    return is_bulk_power_producer(area) or is_pv_node_type(area) or \
+           area['type'] == "FinitePowerPlant"
+
+
 def is_prosumer_node_type(area):
     return area['type'] in ["StorageStrategy", "StorageExternalStrategy"]
 
 
 def is_buffer_node_type(area):
     return area['type'] == "InfiniteBusStrategy"
+
+
+def get_unified_area_type(area):
+    if is_pv_node_type(area):
+        return "PV"
+    elif is_load_node_type(area):
+        return "Load"
+    elif is_cell_tower_type(area):
+        return "CellTower"
+    elif is_prosumer_node_type(area):
+        return "Storage"
+    elif is_bulk_power_producer(area) or is_buffer_node_type(area):
+        return "MarketMaker"
+    elif area['type'] == "FinitePowerPlant":
+        return "FinitePowerPlant"
+    else:
+        return "Area"
 
 
 def area_sells_to_child(trade, area_name, child_names):
