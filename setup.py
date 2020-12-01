22eb9d5a8d96bdac62c7c8351735d92fc15a78df
Lana <svetlana.konstantinovna@gmail.com>
2020-12-01 16:10:44 +0100

    D3ASIM-3075-2: deprecate old blockchain dependencies from dev and tests

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ d3a_interface_branch = os.environ.get("BRANCH", "master")
 try:
     with open('requirements/dev.txt') as req:
         REQUIREMENTS = [r.partition('#')[0] for r in req if not r.startswith('-e')]
-        # TODO: Workaround for https://github.com/ethereum/py-solc/issues/64
         REQUIREMENTS.extend(
             ['d3a-interface @ '
              f'git+https://github.com/gridsingularity/d3a-interface.git@{d3a_interface_branch}'
