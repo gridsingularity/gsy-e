b52a21b33d48b3aa0103da863a9610252d6bf196
Pierpaolo Pantone <24alsecondo@gmail.com>
2020-12-08 15:09:10 +0100

    D3ASIM-3109: remove references to solium in travis files

--- .support/travis-run.sh
+++ .support/travis-run.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$TOXENV" == "solium" ]; then
-	. $HOME/.nvm/nvm.sh
-	nvm use stable
-fi
-
 # Sigh, since travis container infrastructure doesn't support sudo we manually "install" solc
 export SOLC_BINARY=$HOME/solc/usr/bin/solc
 export LD_LIBRARY_PATH=$HOME/solc/usr/lib
