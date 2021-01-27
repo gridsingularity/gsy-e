0304c5f8a345c1006f0824348e40d0e5167945a0
Lana <svetlana.konstantinovna@gmail.com>
2021-01-27 17:43:26 +0100

    DD-7: removed id from substrate call parameters

--- src/d3a/models/market/blockchain_interface.py
+++ src/d3a/models/market/blockchain_interface.py
@@ -99,7 +99,6 @@ class SubstrateBlockchainInterface(BlockChainInterface):
     def track_trade_event(self, trade):
 
         call_params = {
-            'id': 3,
             'buyer': ALICE_STASH_ADDRESS,
             'seller': BOB_STASH_ADDRESS,
             'amount': default_amount,
