4db5f2731d1f12a079e805f14577b5ca1f075f65
Hannes Diedrich <hannes@gridsingularity.com>
2021-03-24 09:47:01 +0100

    D3ASIM-3268: Fixed typo.

--- src/d3a/models/area/stats.py
+++ src/d3a/models/area/stats.py
@@ -72,7 +72,7 @@ class AreaStats:
         if self.current_market is not None:
             self.market_bills = \
                 {self.current_market.time_slot: {
-                    "Accumulated Trades", self._extract_from_bills("Accumulated Trades")}}
+                    "Accumulated Trades": self._extract_from_bills("Accumulated Trades")}}
 
             self.rate_stats_market = {
                 self.current_market.time_slot: self.min_max_avg_median_rate_current_market()
