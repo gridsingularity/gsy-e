2fdf9c2d1a279a59514025e4b05fafb3bb3c271a
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-05-20 17:36:11 +0300

    D3ASIM-3447: Myco redis client

--- src/d3a/models/myco_matcher/external_matcher.py
+++ src/d3a/models/myco_matcher/external_matcher.py
@@ -10,28 +10,30 @@ from d3a.models.myco_matcher.base_matcher import BaseMatcher
 class ExternalMatcher(BaseMatcher):
     def __init__(self):
         super(ExternalMatcher, self).__init__()
-        self.job_id = d3a.constants.COLLABORATION_ID
-        self.myko_ext_conn = None
-        self.channel_prefix = f"external-myko/{self.job_id}/"
+        self.job_uuid = d3a.constants.COLLABORATION_ID
+        self.myco_ext_conn = None
+        self.channel_prefix = f"external-myco/{self.job_uuid}/"
         self._setup_redis_connection()
         self.area_uuid_markets_mapping = {}
         self.markets_mapping = {}
         self.recommendations = []
 
     def _setup_redis_connection(self):
-        self.myko_ext_conn = RedisCommunicator()
-        self.myko_ext_conn.sub_to_channel(f"{self.channel_prefix}get_offers_bids/",
-                                          self.get_offers_bids)
-        self.myko_ext_conn.sub_to_channel(f"{self.channel_prefix}post_recommendations/",
+        self.myco_ext_conn = RedisCommunicator()
+        self.myco_ext_conn.sub_to_channel("external-myco/get_job_uuid",
+                                          self.get_job_uuid)
+        self.myco_ext_conn.sub_to_channel(f"{self.channel_prefix}get_offers_bids/",
+                                          self.publish_offers_bids)
+        self.myco_ext_conn.sub_to_channel(f"{self.channel_prefix}post_recommendations/",
                                           self.match_recommendations)
 
-    def get_offers_bids(self, payload):
+    def publish_offers_bids(self, message):
         """
         Function that queries publishes open offers and bids
         published data are of the following format
         market_offers_bids_list_mapping = {"market_id" : {"bids": [], "offers": [] }, }
         """
-        # TODO: payload can contain filters
+        # TODO: message can contain filters
         data = {"event": "offers_bids_response"}
         market_offers_bids_list_mapping = {}
         for area_uuid, markets in self.area_uuid_markets_mapping.items():
@@ -47,14 +49,11 @@ class ExternalMatcher(BaseMatcher):
             "market_offers_bids_list_mapping": market_offers_bids_list_mapping,
         })
 
-        self.publish_offers_bids(data)
+        channel = f"{self.channel_prefix}response/get_offers_bids/"
+        self.myco_ext_conn.publish(channel, json.dumps(data))
 
-    def publish_offers_bids(self, data):
-        channel = f"{self.channel_prefix}get_offers_bids/response/"
-        self.myko_ext_conn.publish(channel, json.dumps(data))
-
-    def match_recommendations(self, payload):
-        for record in payload["data"]:
+    def match_recommendations(self, message):
+        for record in message["data"]:
             market = self.markets_mapping[record["market_id"]]
             validate_authentic_bid_offer_pair(
                 record.get("bid"),
@@ -66,6 +65,12 @@ class ExternalMatcher(BaseMatcher):
                                                         record.get("selected_energy"),
                                                         record.get("offer"),
                                                         record.get("trade_rate")), ])
+        channel = f"{self.channel_prefix}response/matched_recommendations/"
+        self.myco_ext_conn.publish(channel, json.dumps({"event": "match", "status": "success"}))
 
     def calculate_match_recommendation(self, bids, offers, current_time=None):
         pass
+
+    def get_job_uuid(self, message):
+        channel = "external-myco/get_job_uuid/response"
+        self.myco_ext_conn.publish(channel, json.dumps({"job_uuid": self.job_uuid}))
