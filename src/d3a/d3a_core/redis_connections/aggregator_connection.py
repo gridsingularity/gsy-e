ceb0cd569ebd369540e69c5c502f993157bd94fb
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-06-07 10:16:32 +0300

    D3ASIM-3447: Fix indentation

--- src/d3a/d3a_core/redis_connections/aggregator_connection.py
+++ src/d3a/d3a_core/redis_connections/aggregator_connection.py
@@ -67,7 +67,7 @@ class AggregatorHandler:
     def _create_grid_tree_event_dict(self, aggregator_uuid: str) -> dict:
         """Accumulate area_stats_tree_dict information and initiate a event dictionary
         to be sent to the client"""
-              from d3a.d3a_core.singletons import external_global_statistics
+        from d3a.d3a_core.singletons import external_global_statistics
         return {"grid_tree": self._delete_not_owned_devices_from_dict(
             external_global_statistics.area_stats_tree_dict, aggregator_uuid),
                 "feed_in_tariff_rate": external_global_statistics.current_feed_in_tariff,
