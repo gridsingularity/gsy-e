2bbb4913b5af15572eed40cab7003185b0d33fe0
faizan2590 <faizan2590@gmail.com>
2021-06-30 18:08:43 +0500

    D3ASIM-3464: Adding integration test for family hous with PV+ESS.

--- integration_tests/savings_kpi.feature
+++ integration_tests/savings_kpi.feature
@@ -10,4 +10,10 @@ Feature: Savings KPI integration tests
      Given we have a scenario named kpi.d3asim_3464_family_with_pv
      And d3a is installed
      When we run the d3a simulation on console with kpi.d3asim_3464_family_with_pv for 24 hrs
-     Then saving_percentage of {'Family 2 children with PV': -43.607} are correctly reported
+     Then saving_percentage of {'Family 2 children with PV': -43.537} are correctly reported
+
+  Scenario: Family House with PV & ESS
+     Given we have a scenario named kpi.d3asim_3464_family_with_pv_ess
+     And d3a is installed
+     When we run the d3a simulation on console with kpi.d3asim_3464_family_with_pv_ess for 24 hrs
+     Then saving_percentage of {'Family 2 children with PV + ESS': -176.514} are correctly reported
--- src/d3a/setup/kpi/d3asim_3464_family_with_pv_ess.py
+++ src/d3a/setup/kpi/d3asim_3464_family_with_pv_ess.py
@@ -3,6 +3,7 @@ from d3a_interface.constants_limits import ConstSettings
 from d3a.models.strategy.infinite_bus import InfiniteBusStrategy
 from d3a.models.strategy.load_hours import LoadHoursStrategy
 from d3a.models.strategy.pv import PVStrategy
+from d3a.models.strategy.storage import StorageStrategy
 from d3a.models.strategy.finite_power_plant import FinitePowerPlant
 
 
@@ -18,19 +19,25 @@ def get_setup(config):
             Area(
                 'Community',
                 [
-                    Area('Family 2 children with PV',
+                    Area('Family 2 children with PV + ESS',
                          [
                             Area('Family General Load', strategy=LoadHoursStrategy(
-                                avg_power_W=2161, hrs_of_day=list(range(24)), hrs_per_day=24,
-                                initial_buying_rate=27.895, final_buying_rate=27.895,
-                                fit_to_limit=True)),
+                                avg_power_W=5680, hrs_of_day=list(range(24)), hrs_per_day=24,
+                                initial_buying_rate=23, final_buying_rate=23)),
                             Area('Family PV', strategy=PVStrategy(
                                 max_panel_power_W=17985, panel_count=1,
-                                initial_selling_rate=5, final_selling_rate=5))
+                                initial_selling_rate=6, final_selling_rate=6)),
+                            Area('Family ESS', strategy=StorageStrategy(
+                                 initial_soc=20, battery_capacity_kWh=5,
+                                 initial_buying_rate=22.94, final_buying_rate=22.94,
+                                 initial_selling_rate=22.95, final_selling_rate=22.95))
                          ]),
                     Area('Community PP', strategy=FinitePowerPlant(
-                        max_available_power_kW=5, energy_rate=1
-                    ))
+                        max_available_power_kW=10, energy_rate=1
+                    )),
+                    Area('Community Load', strategy=LoadHoursStrategy(
+                        avg_power_W=1200, hrs_of_day=list(range(24)), hrs_per_day=24,
+                        initial_buying_rate=10, final_buying_rate=10))
 
                 ], grid_fee_constant=4.0),
             Area('DSO', strategy=InfiniteBusStrategy(energy_buy_rate=20, energy_sell_rate=22))
