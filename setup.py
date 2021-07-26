b7acd59cea2020ffa93f4e6dc4b1d13f2dc63978
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-07-26 14:13:20 +0300

    D3ASIM-3532: Remove d3a-interface explicit installation in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 from setuptools import find_packages, setup
-import os
 
-
-d3a_interface_branch = os.environ.get("BRANCH", "feature/D3ASIM-3532")
 
 try:
-    with open('requirements/dev.txt') as req:
-        REQUIREMENTS = [r.partition('#')[0] for r in req if not r.startswith('-e')]
-        REQUIREMENTS.extend(
-            [f'd3a-interface @ '
-             f'git+https://github.com/gridsingularity/d3a-interface.git@{d3a_interface_branch}'
-             ])
+    with open("requirements/dev.txt") as req:
+        REQUIREMENTS = [r.partition("#")[0] for r in req if not r.startswith("-e")]
 except OSError:
     # Shouldn't happen
     REQUIREMENTS = []
@@ -19,23 +12,23 @@ with open("README.rst", "r") as readme:
     README = readme.read()
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-VERSION = '1.0.0'
+VERSION = "1.0.0"
 
 setup(
     name="d3a",
     description="decentralised energy exchange developed by Grid Singularity",
     long_description=README,
-    author='GridSingularity',
-    author_email='d3a@gridsingularity.com',
-    url='https://github.com/gridsingularity/d3a',
+    author="GridSingularity",
+    author_email="d3a@gridsingularity.com",
+    url="https://github.com/gridsingularity/d3a",
     version=VERSION,
     packages=find_packages(where="src", exclude=["tests"]),
     package_dir={"": "src"},
-    package_data={'d3a': ['resources/*.csv']},
+    package_data={"d3a": ["resources/*.csv"]},
     install_requires=REQUIREMENTS,
     entry_points={
-        'console_scripts': [
-            'd3a = d3a.d3a_core.cli:main',
+        "console_scripts": [
+            "d3a = d3a.d3a_core.cli:main",
         ]
     },
     zip_safe=False,
