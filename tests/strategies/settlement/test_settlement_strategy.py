b5f00b1025368b33282c18ce5e101cb6f38d01c0
Hannes Diedrich <hannes@gridsingularity.com>
2021-09-02 15:53:23 +0200

    D3ASIM-3539: Fixed settlement market tamplate startegy unit tests.

--- tests/strategies/settlement/test_settlement_strategy.py
+++ tests/strategies/settlement/test_settlement_strategy.py
@@ -45,6 +45,10 @@ class TestSettlementMarketStrategy:
         strategy_fixture.area = Mock()
         strategy_fixture.area.settlement_markets = self.settlement_markets
         strategy_fixture.get_market_from_id = MagicMock(return_value=self.market_mock)
+        strategy_fixture.area.current_tick = 0
+        strategy_fixture.area.config = Mock()
+        strategy_fixture.area.config.ticks_per_slot = 60
+        strategy_fixture.area.config.tick_length = duration(seconds=15)
 
     def teardown_method(self):
         ConstSettings.SettlementMarketSettings.ENABLE_SETTLEMENT_MARKETS = False
