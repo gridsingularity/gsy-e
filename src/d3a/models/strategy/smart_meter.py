f1990f5e799e232496f60f9a5ebc39cd86f5ccfa
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-09-06 14:16:27 +0300

    D3ASIM-3556: Fix merge conflict

--- src/d3a/models/strategy/smart_meter.py
+++ src/d3a/models/strategy/smart_meter.py
@@ -32,7 +32,6 @@ from d3a.d3a_core.exceptions import MarketException
 from d3a.d3a_core.util import get_market_maker_rate_from_config
 from d3a.models.market import Market
 from d3a.models.state import SmartMeterState
-from d3a.models.state import HomeMeterState
 from d3a.models.strategy import BidEnabledStrategy, utils
 from d3a.models.strategy.update_frequency import (
     TemplateStrategyBidUpdater, TemplateStrategyOfferUpdater)
