ed303249c3fdd083210ff5ebf2472f506321973c
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-10-14 10:56:57 +0300

    Create is_subscribed_to_channels_patterns property in the RedisCommunicator

--- src/d3a/d3a_core/redis_connections/redis_area_market_communicator.py
+++ src/d3a/d3a_core/redis_connections/redis_area_market_communicator.py
@@ -59,6 +59,11 @@ class RedisCommunicator:
         log.trace(f"Started thread for events: {thread}")
         return thread
 
+    @property
+    def is_subscribed_to_channels_patterns(self) -> bool:
+        """Check whether the communicator subscribed to at least 1 channel or pattern."""
+        return self.pubsub.channels or self.pubsub.patterns
+
 
 class ResettableCommunicator(RedisCommunicator):
     def __init__(self):
@@ -116,7 +121,7 @@ class ExternalConnectionCommunicator(ResettableCommunicator):
     def start_communication(self):
         if not self.is_enabled:
             return
-        if not (self.pubsub.channels or self.pubsub.patterns):
+        if not self.is_subscribed_to_channels_patterns:
             return
         thread = self.pubsub.run_in_thread(sleep_time=0.1, daemon=True)
         log.debug(f"Started ExternalConnectionCommunicator thread for "
