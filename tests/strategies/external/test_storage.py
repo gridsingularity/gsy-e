cd056c108a5b3e108c52c44686f994bfee2a6d21
Pierpaolo Pantone <24alsecondo@gmail.com>
2021-10-25 11:57:03 +0200

    D3ASIM-3626-refactoring: Refactor tests to remove pylint warnings

--- tests/strategies/external/test_storage.py
+++ tests/strategies/external/test_storage.py
@@ -17,42 +17,48 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import pytest
-from d3a.models.strategy.external_strategies.storage import StorageExternalStrategy
 
+from d3a.models.strategy.external_strategies.storage import StorageExternalStrategy
 from tests.strategies.external.utils import (
     check_external_command_endpoint_with_correct_payload_succeeds,
     create_areas_markets_for_strategy_fixture)
 
 
-@pytest.fixture
-def ext_storage_fixture():
+@pytest.fixture(name="external_storage")
+def external_storage_fixture():
     return create_areas_markets_for_strategy_fixture(StorageExternalStrategy())
 
 
 class TestPVForecastExternalStrategy:
 
-    def test_offer_succeeds(self, ext_storage_fixture):
+    @staticmethod
+    def test_offer_succeeds(external_storage):
         arguments = {"price": 1, "energy": 2}
-        check_external_command_endpoint_with_correct_payload_succeeds(ext_storage_fixture,
-                                                                      "offer", arguments)
+        check_external_command_endpoint_with_correct_payload_succeeds(
+            external_storage, "offer", arguments)
 
-    def test_list_offers_succeeds(self, ext_storage_fixture):
-        check_external_command_endpoint_with_correct_payload_succeeds(ext_storage_fixture,
-                                                                      "list_offers", {})
+    @staticmethod
+    def test_list_offers_succeeds(external_storage):
+        check_external_command_endpoint_with_correct_payload_succeeds(
+            external_storage, "list_offers", {})
 
-    def test_delete_offer_succeeds(self, ext_storage_fixture):
-        check_external_command_endpoint_with_correct_payload_succeeds(ext_storage_fixture,
-                                                                      "delete_offer", {})
+    @staticmethod
+    def test_delete_offer_succeeds(external_storage):
+        check_external_command_endpoint_with_correct_payload_succeeds(
+            external_storage, "delete_offer", {})
 
-    def test_bid_succeeds(self, ext_storage_fixture):
+    @staticmethod
+    def test_bid_succeeds(external_storage):
         arguments = {"price": 1, "energy": 2}
-        check_external_command_endpoint_with_correct_payload_succeeds(ext_storage_fixture,
-                                                                      "bid", arguments)
+        check_external_command_endpoint_with_correct_payload_succeeds(
+            external_storage, "bid", arguments)
 
-    def test_list_bids_succeeds(self, ext_storage_fixture):
-        check_external_command_endpoint_with_correct_payload_succeeds(ext_storage_fixture,
-                                                                      "list_bids", {})
+    @staticmethod
+    def test_list_bids_succeeds(external_storage):
+        check_external_command_endpoint_with_correct_payload_succeeds(
+            external_storage, "list_bids", {})
 
-    def test_delete_bid_succeeds(self, ext_storage_fixture):
-        check_external_command_endpoint_with_correct_payload_succeeds(ext_storage_fixture,
-                                                                      "delete_bid", {})
+    @staticmethod
+    def test_delete_bid_succeeds(external_storage):
+        check_external_command_endpoint_with_correct_payload_succeeds(
+            external_storage, "delete_bid", {})
