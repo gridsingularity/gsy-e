412551d1f64bfbfbe56d5a1ecbdad1fe80a3c9db
spyrostz <spyros@gridsingularity.com>
2022-01-20 13:27:16 +0100

    GSYE-117: Refactored the StorageState and its calling code in order to move more state-related parts in the state class from the storage strategy class.

--- src/gsy_e/models/state.py
+++ src/gsy_e/models/state.py
@@ -23,8 +23,8 @@ from typing import Dict, Optional, List
 
 from gsy_framework.constants_limits import ConstSettings, GlobalConfig
 from gsy_framework.utils import (
-    convert_pendulum_to_str_in_dict, convert_str_to_pendulum_in_dict, convert_kW_to_kWh)
-from gsy_framework.utils import limit_float_precision
+    convert_pendulum_to_str_in_dict, convert_str_to_pendulum_in_dict, convert_kW_to_kWh,
+    limit_float_precision)
 from pendulum import DateTime
 
 from gsy_e.constants import FLOATING_POINT_TOLERANCE
@@ -541,15 +541,6 @@ class StorageState(StateInterface):
         """
         return self._used_storage
 
-    def update_used_storage_share(self, energy: float, source=ESSEnergyOrigin.UNKNOWN) -> None:
-        """Add an EnergyOrigin instance to the _used_storage_share list."""
-        self._used_storage_share.append(EnergyOrigin(source, energy))
-
-    @property
-    def get_used_storage_share(self) -> List:
-        """Return the _used_storage_share list."""
-        return self._used_storage_share
-
     def free_storage(self, time_slot):
         """
         Storage, that has not been promised or occupied
@@ -559,12 +550,12 @@ class StorageState(StateInterface):
                   + self.pledged_buy_kWh[time_slot])
         return self.capacity - in_use
 
-    def max_offer_energy_kWh(self, time_slot: DateTime) -> float:
+    def _max_offer_energy_kWh(self, time_slot: DateTime) -> float:
         """Return the max tracked offered energy."""
         return (self._battery_energy_per_slot - self.pledged_sell_kWh[time_slot]
                 - self.offered_sell_kWh[time_slot])
 
-    def max_buy_energy_kWh(self, time_slot: DateTime) -> float:
+    def _max_buy_energy_kWh(self, time_slot: DateTime) -> float:
         """Return the min tracked bid energy."""
         return (self._battery_energy_per_slot - self.pledged_buy_kWh[time_slot]
                 - self.offered_buy_kWh[time_slot])
@@ -574,7 +565,7 @@ class StorageState(StateInterface):
         self._battery_energy_per_slot = convert_kW_to_kWh(self.max_abs_battery_power_kW,
                                                           slot_length)
 
-    def has_battery_reached_max_power(self, energy: float, time_slot: DateTime) -> bool:
+    def _has_battery_reached_max_power(self, energy: float, time_slot: DateTime) -> bool:
         """Check whether the storage can withhold the passed energy value."""
         return limit_float_precision(abs(energy
                                      + self.pledged_sell_kWh[time_slot]
@@ -583,7 +574,7 @@ class StorageState(StateInterface):
                                      - self.offered_buy_kWh[time_slot])
                                      ) > self._battery_energy_per_slot
 
-    def clamp_energy_to_sell_kWh(self, market_slot_time_list):
+    def _clamp_energy_to_sell_kWh(self, market_slot_time_list):
         """
         Determines available energy to sell for each active market and returns a dict[TIME, FLOAT]
         """
@@ -601,13 +592,13 @@ class StorageState(StateInterface):
         for time_slot in market_slot_time_list:
             storage_dict[time_slot] = limit_float_precision(min(
                                                             energy / len(market_slot_time_list),
-                                                            self.max_offer_energy_kWh(time_slot),
+                                                            self._max_offer_energy_kWh(time_slot),
                                                             self._battery_energy_per_slot))
             self.energy_to_sell_dict[time_slot] = storage_dict[time_slot]
 
         return storage_dict
 
-    def clamp_energy_to_buy_kWh(self, market_slot_time_list):
+    def _clamp_energy_to_buy_kWh(self, market_slot_time_list):
         """
         Determines amount of energy that can be bought for each active market and writes it to
         self.energy_to_buy_dict
@@ -625,7 +616,7 @@ class StorageState(StateInterface):
 
         for time_slot in market_slot_time_list:
             clamped_energy = limit_float_precision(
-                min(energy, self.max_buy_energy_kWh(time_slot), self._battery_energy_per_slot))
+                min(energy, self._max_buy_energy_kWh(time_slot), self._battery_energy_per_slot))
             clamped_energy = max(clamped_energy, 0)
             self.energy_to_buy_dict[time_slot] = clamped_energy
 
@@ -633,6 +624,9 @@ class StorageState(StateInterface):
         """
         Sanity check of the state variables.
         """
+        self._clamp_energy_to_sell_kWh([time_slot])
+        self._clamp_energy_to_buy_kWh([time_slot])
+        self._calculate_soc_for_time_slot(time_slot)
         charge = limit_float_precision(self.used_storage / self.capacity)
         max_value = self.capacity - self.min_allowed_soc_ratio * self.capacity
         assert self.min_allowed_soc_ratio <= charge or \
@@ -646,7 +640,7 @@ class StorageState(StateInterface):
         assert 0 <= limit_float_precision(self.pledged_buy_kWh[time_slot]) <= max_value
         assert 0 <= limit_float_precision(self.offered_buy_kWh[time_slot]) <= max_value
 
-    def calculate_soc_for_time_slot(self, time_slot: DateTime) -> None:
+    def _calculate_soc_for_time_slot(self, time_slot: DateTime) -> None:
         """Update the soc history for the passed time_slot."""
         self.charge_history[time_slot] = 100.0 * self.used_storage / self.capacity
         self.charge_history_kWh[time_slot] = self.used_storage
@@ -689,7 +683,10 @@ class StorageState(StateInterface):
             self._used_storage -= self.pledged_sell_kWh[past_time_slot]
             self._used_storage += self.pledged_buy_kWh[past_time_slot]
 
-        self.calculate_soc_for_time_slot(current_time_slot)
+        self._clamp_energy_to_sell_kWh([current_time_slot, *all_future_time_slots])
+        self._clamp_energy_to_buy_kWh([current_time_slot, *all_future_time_slots])
+        self._calculate_soc_for_time_slot(current_time_slot)
+
         self.offered_history[current_time_slot] = self.offered_sell_kWh[current_time_slot]
 
         if past_time_slot:
@@ -697,6 +694,10 @@ class StorageState(StateInterface):
                 self.time_series_ess_share[past_time_slot][energy_type.origin] += energy_type.value
 
     def delete_past_state_values(self, current_time_slot: DateTime):
+        """
+        Clean up values from past market slots that are not used anymore. Useful for
+        deallocating memory that is not used anymore.
+        """
         to_delete = []
         for market_slot in self.pledged_sell_kWh:
             if is_time_slot_in_past_markets(market_slot, current_time_slot):
@@ -712,6 +713,110 @@ class StorageState(StateInterface):
             self.used_history.pop(market_slot, None)
             self.energy_to_buy_dict.pop(market_slot, None)
             self.energy_to_sell_dict.pop(market_slot, None)
+
+    def register_energy_from_posted_bid(self, energy: float, time_slot: DateTime):
+        """Register the energy from a posted bid on the market."""
+        self.offered_buy_kWh[time_slot] += energy
+
+    def register_energy_from_posted_offer(self, energy: float, time_slot: DateTime):
+        """Register the energy from a posted offer on the market."""
+        self.offered_sell_kWh[time_slot] += energy
+
+    def reset_offered_sell_energy(self, energy: float, time_slot: DateTime):
+        """Reset the offered sell energy amount."""
+        self.offered_sell_kWh[time_slot] = energy
+        self._clamp_energy_to_sell_kWh([time_slot])
+
+    def reset_offered_buy_energy(self, energy: float, time_slot: DateTime):
+        """Reset the offered buy energy amount."""
+        self.offered_buy_kWh[time_slot] = energy
+        self._clamp_energy_to_buy_kWh([time_slot])
+
+    def remove_energy_from_deleted_offer(self, energy: float, time_slot: DateTime):
+        """
+        Unregister the energy from a deleted offer, in order to be available for following offers.
+        """
+        self.offered_sell_kWh[time_slot] -= energy
+
+    def register_energy_from_one_sided_market_accept_offer(
+            self, energy: float, time_slot: DateTime,
+            energy_origin: ESSEnergyOrigin = ESSEnergyOrigin.UNKNOWN):
+        """
+        Register energy from one sided market accept offer operation. Similar behavior as
+        register_energy_from_bid_trade with the exception that offered_buy_kWh is not used in
+        one-sided markets (because there is no concept of bid, the storage never actually offers
+        energy, the buyers with access to the market can accept it).
+        """
+        self.pledged_buy_kWh[time_slot] += energy
+        self._track_energy_bought_type(energy, energy_origin)
+
+    def register_energy_from_bid_trade(
+            self, energy: float, time_slot: DateTime,
+            energy_origin: ESSEnergyOrigin = ESSEnergyOrigin.UNKNOWN):
+        """Register energy from a bid trade event."""
+        self.pledged_buy_kWh[time_slot] += energy
+        self.offered_buy_kWh[time_slot] -= energy
+        self._track_energy_bought_type(energy, energy_origin)
+
+    def register_energy_from_offer_trade(self, energy: float, time_slot: DateTime):
+        """Register energy from an offer trade event."""
+        self.pledged_sell_kWh[time_slot] += energy
+        self.offered_sell_kWh[time_slot] -= energy
+        self._track_energy_sell_type(energy)
+
+    def _track_energy_bought_type(self, energy: float, energy_origin: ESSEnergyOrigin):
+        self._used_storage_share.append(EnergyOrigin(energy_origin, energy))
+
+    # ESS Energy being utilized based on FIRST-IN FIRST-OUT mechanism
+    def _track_energy_sell_type(self, energy: float):
+        while limit_float_precision(energy) > 0 and len(self._used_storage_share) > 0:
+            first_in_energy_with_origin = self._used_storage_share[0]
+            if energy >= first_in_energy_with_origin.value:
+                energy -= first_in_energy_with_origin.value
+                self._used_storage_share.pop(0)
+            elif energy < first_in_energy_with_origin.value:
+                residual = first_in_energy_with_origin.value - energy
+                self._used_storage_share[0] = EnergyOrigin(
+                    first_in_energy_with_origin.origin, residual)
+                energy = 0
+
+    def get_available_energy_to_buy_kWh(self, time_slot: DateTime) -> float:
+        """Retrieve the amount of energy that the storage can buy in the given time slot."""
+        if self.free_storage(time_slot) == 0:
+            return 0.0
+
+        self._clamp_energy_to_buy_kWh([time_slot])
+        energy_kWh = self.energy_to_buy_dict[time_slot]
+        if self._has_battery_reached_max_power(abs(energy_kWh), time_slot):
+            return 0.0
+        return energy_kWh
+
+    def get_available_energy_to_sell_kWh(self, time_slot: DateTime) -> float:
+        """Retrieve the amount of energy that the storage can sell in the given time slot."""
+        if self.used_storage == 0:
+            return 0.0
+        energy_sell_dict = self._clamp_energy_to_sell_kWh([time_slot])
+        energy_kWh = energy_sell_dict[time_slot]
+        if self._has_battery_reached_max_power(energy_kWh, time_slot):
+            return 0.0
+        return energy_kWh
+
+    def get_soc_level(self, time_slot: DateTime) -> float:
+        """Get the SOC level of the storage, in percentage units."""
+        if self.charge_history[time_slot] == "-":
+            return self.used_storage / self.capacity
+        return self.charge_history[time_slot] / 100.0
+
+    def to_dict(self, time_slot: DateTime) -> Dict:
+        """Get a dict with the current stats of the storage according to timeslot."""
+        return {
+            "energy_to_sell": self.energy_to_sell_dict[time_slot],
+            "energy_active_in_bids": self.offered_sell_kWh[time_slot],
+            "energy_to_buy": self.energy_to_buy_dict[time_slot],
+            "energy_active_in_offers": self.offered_buy_kWh[time_slot],
+            "free_storage": self.free_storage(time_slot),
+            "used_storage": self.used_storage,
+        }
 
 
 class UnexpectedStateException(Exception):
