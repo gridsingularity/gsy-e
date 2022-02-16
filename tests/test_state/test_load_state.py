9925542894ee5a82654b0baf1b741f6242fc85c8
ERafaelMartinez <eduardorafael@gridsingularity.com>
2022-02-16 09:40:02 -0600

    TESTS-111: Fix LoadState instance.

--- tests/test_state/test_load_state.py
+++ tests/test_state/test_load_state.py
@@ -1,7 +1,7 @@
 # pylint: disable=protected-access
 from pendulum import now
 
-from gsy_e.models.state import PVState
+from gsy_e.models.state import LoadState
 from tests.test_state.test_prosumption_interface import TestProsumptionInterface
 
 
@@ -9,17 +9,17 @@ class TestPVState(TestProsumptionInterface):
 
     @staticmethod
     def _setup_base_configuration():
-        return PVState(), now()
+        return LoadState(), now()
 
     @staticmethod
     def _setup_configuration_for_settlement_posting(
             energy_deviation=None, unsettled_deviation=None):
-        pv_state = PVState()
+        load_state = LoadState()
         current_time_slot = now()
         if energy_deviation:
-            pv_state._forecast_measurement_deviation_kWh[
+            load_state._forecast_measurement_deviation_kWh[
                 current_time_slot] = energy_deviation
         if unsettled_deviation:
-            pv_state._unsettled_deviation_kWh[
+            load_state._unsettled_deviation_kWh[
                 current_time_slot] = unsettled_deviation
-        return pv_state, current_time_slot
+        return load_state, current_time_slot
