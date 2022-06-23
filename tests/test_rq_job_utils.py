137adb3bba43a6cea4f2d88bcf7ff50a417863b2
Amirhossein Saemi <amirhossein@gridsingularity.com>
2022-06-23 06:04:09 +0200

    GSYE-324: Make test_launch_simulation_from_rq_job run faster

--- tests/test_rq_job_utils.py
+++ tests/test_rq_job_utils.py
@@ -1,6 +1,8 @@
 import pickle
 import zlib
 
+from pendulum import duration
+
 from gsy_e.gsy_e_core.rq_job_handler import launch_simulation_from_rq_job
 
 
@@ -11,7 +13,11 @@ class TestRQJobUtils:
         """Assure the launch_simulation_from_rq_job can successfully launch a simulation."""
         launch_simulation_from_rq_job(
             scenario=zlib.compress(pickle.dumps("default_2a")),
-            settings=None,
+            settings={
+                "sim_duration": duration(days=1),
+                "slot_length": duration(hours=1),
+                "tick_length": duration(minutes=6)
+            },
             events=None,
             aggregator_device_mapping="null",
             saved_state=zlib.compress(pickle.dumps(None)),
