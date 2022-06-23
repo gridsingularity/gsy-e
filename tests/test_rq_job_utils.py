c578eecc022b6512e638c9d88d8a5ae63562f05f
Amirhossein Saemi <amirhossein@gridsingularity.com>
2022-06-23 06:42:05 +0200

    GSYE-324: Explicitly failing test_launch_simulation_from_rq_job in case of a problem

--- tests/test_rq_job_utils.py
+++ tests/test_rq_job_utils.py
@@ -1,6 +1,9 @@
+# pylint: disable=broad-except
+
 import pickle
 import zlib
 
+import pytest
 from pendulum import duration
 
 from gsy_e.gsy_e_core.rq_job_handler import launch_simulation_from_rq_job
@@ -11,16 +14,19 @@ class TestRQJobUtils:
     @staticmethod
     def test_launch_simulation_from_rq_job():
         """Assure the launch_simulation_from_rq_job can successfully launch a simulation."""
-        launch_simulation_from_rq_job(
-            scenario=zlib.compress(pickle.dumps("default_2a")),
-            settings={
-                "sim_duration": duration(days=1),
-                "slot_length": duration(hours=1),
-                "tick_length": duration(minutes=6)
-            },
-            events=None,
-            aggregator_device_mapping="null",
-            saved_state=zlib.compress(pickle.dumps(None)),
-            job_id="TEST_SIM_RUNS",
-            connect_to_profiles_db=False
-        )
+        try:
+            launch_simulation_from_rq_job(
+                scenario=zlib.compress(pickle.dumps("default_2a")),
+                settings={
+                    "duration": duration(days=1),
+                    "slot_length": duration(hours=1),
+                    "tick_length": duration(minutes=6)
+                },
+                events=None,
+                aggregator_device_mapping="null",
+                saved_state=zlib.compress(pickle.dumps(None)),
+                job_id="TEST_SIM_RUNS",
+                connect_to_profiles_db=False
+            )
+        except Exception as exc: # noqa
+            pytest.fail(exc)
