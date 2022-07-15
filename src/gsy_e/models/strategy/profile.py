ec5426bb28c262a299798e9631d4ee47914075bf
Amirhossein Saemi <amirhossein@gridsingularity.com>
2022-07-15 09:09:33 +0200

    GSYE-12: Add support for input_energy_rate for EnergyProfile class

--- src/gsy_e/models/strategy/profile.py
+++ src/gsy_e/models/strategy/profile.py
@@ -1,41 +1,46 @@
-from gsy_framework.read_user_profile import InputProfileTypes
+from gsy_framework.read_user_profile import InputProfileTypes, convert_identity_profile_to_float
 
-import gsy_e.constants
 from gsy_e.gsy_e_core.global_objects_singleton import global_objects
+from gsy_e.gsy_e_core.util import should_read_profile_from_db
 
 
 class EnergyProfile:
     """Manage reading/rotating energy profile of an asset."""
-    def __init__(self, input_profile, input_profile_uuid):
+    def __init__(self, input_profile=None, input_profile_uuid=None, input_energy_rate=None):
         self.input_profile = input_profile
         self.input_profile_uuid = input_profile_uuid
+        self.input_energy_rate = input_energy_rate
 
-        # ignore input_profile if profile should be fetched from db.
-        if self.should_read_profile_from_db(self.input_profile_uuid):
+        if should_read_profile_from_db(self.input_profile_uuid):
             self.input_profile = None
+            self.input_energy_rate = None
+        elif input_energy_rate:
+            self.input_profile = None
+            self.input_profile_uuid = None
+        else:
+            self.input_profile_uuid = None
+            self.input_energy_rate = None
 
         self.profile = None
 
-    @staticmethod
-    def should_read_profile_from_db(profile_uuid) -> bool:
-        """Boolean return if profile to be read from DB."""
-        return profile_uuid is not None and gsy_e.constants.CONNECT_TO_PROFILES_DB
-
-    @property
-    def has_profile(self) -> bool:
-        """Checks if the asset has a predefined profile."""
-        return self.input_profile or self.input_profile_uuid
-
-    def read_or_rotate_profiles(
-            self, reconfigure=False,
-            profile_type: InputProfileTypes = InputProfileTypes.POWER):
+    def read_or_rotate_profiles(self, reconfigure=False):
         """Rotate current profile or read and preprocess profile from source."""
         if not self.profile or reconfigure:
-            profile = self.input_profile
+            profile = self.input_energy_rate or self.input_profile
         else:
             profile = self.profile
 
-        self.profile = global_objects.profiles_handler.rotate_profile(
+        if self.input_energy_rate is not None:
+            profile_type = InputProfileTypes.IDENTITY
+        else:
+            profile_type = InputProfileTypes.POWER
+
+        profile = global_objects.profiles_handler.rotate_profile(
             profile_type=profile_type,
             profile=profile,
             profile_uuid=self.input_profile_uuid)
+
+        if profile_type == InputProfileTypes.IDENTITY:
+            self.profile = convert_identity_profile_to_float(profile)
+        else:
+            self.profile = profile
