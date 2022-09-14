54419866728d41142ab0868d134404f38d648878
Maheen12 <maheenmuneeb12@gmail.com>
2022-09-14 14:08:06 +0200

    GSYE-339:Accept scenario only as a dict in launch_simulation_from_rq_job

--- src/gsy_e/gsy_e_core/rq_job_handler.py
+++ src/gsy_e/gsy_e_core/rq_job_handler.py
@@ -11,7 +11,7 @@ from pendulum import duration, instance
 
 import gsy_e.constants
 from gsy_e.gsy_e_core.simulation import run_simulation
-from gsy_e.gsy_e_core.util import available_simulation_scenarios, update_advanced_settings
+from gsy_e.gsy_e_core.util import update_advanced_settings
 from gsy_e.models.config import SimulationConfig
 
 logger = logging.getLogger(__name__)
@@ -19,7 +19,7 @@ logger.setLevel(logging.INFO)
 
 
 # pylint: disable=too-many-branches, too-many-statements
-def launch_simulation_from_rq_job(scenario: Optional[Dict],
+def launch_simulation_from_rq_job(scenario: Dict,
                                   settings: Optional[Dict],
                                   events: Optional[str],
                                   aggregator_device_mapping: str,
@@ -29,12 +29,12 @@ def launch_simulation_from_rq_job(scenario: Optional[Dict],
     # pylint: disable=too-many-arguments, too-many-locals
     """Launch simulation from rq job."""
     logging.getLogger().setLevel(logging.ERROR)
-    if isinstance(scenario, dict):
-        gsy_e.constants.CONFIGURATION_ID = scenario.pop("configuration_uuid")
-        if "collaboration_uuid" in scenario:
-            gsy_e.constants.EXTERNAL_CONNECTION_WEB = True
-            GlobalConfig.IS_CANARY_NETWORK = scenario.pop("is_canary_network", False)
-            gsy_e.constants.RUN_IN_REALTIME = GlobalConfig.IS_CANARY_NETWORK
+    assert isinstance(scenario, dict)
+    gsy_e.constants.CONFIGURATION_ID = scenario.pop("configuration_uuid")
+    if "collaboration_uuid" in scenario:
+        gsy_e.constants.EXTERNAL_CONNECTION_WEB = True
+        GlobalConfig.IS_CANARY_NETWORK = scenario.pop("is_canary_network", False)
+        gsy_e.constants.RUN_IN_REALTIME = GlobalConfig.IS_CANARY_NETWORK
     logger.info("Starting simulation with job_id: %s and configuration id: %s",
                 job_id, gsy_e.constants.CONFIGURATION_ID)
 
@@ -109,13 +109,8 @@ def launch_simulation_from_rq_job(scenario: Optional[Dict],
             ConstSettings.SettlementMarketSettings.ENABLE_SETTLEMENT_MARKETS
         )
 
-        if scenario is None:
-            scenario_name = "default_2a"
-        elif scenario in available_simulation_scenarios:
-            scenario_name = scenario
-        else:
-            scenario_name = "json_arg"
-            config.area = scenario
+        scenario_name = "json_arg"
+        config.area = scenario
 
         kwargs = {"no_export": True,
                   "seed": settings.get("random_seed", 0)}
