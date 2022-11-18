be0f5d980e9125bde5aa35795ce35f1264cdc37e
Hannes <hannes@gridsingularity.com>
2022-11-18 16:01:52 +0100

    GSYE-450: Add unit test in order to test if the profile uuids are stored into the strategy attributes

---
+++ tests/strategies/test_strategy_inheritance.py
@@ -0,0 +1,46 @@
+from unittest.mock import Mock
+
+import pytest
+
+from gsy_e.models.strategy.external_strategies.load import (
+    LoadProfileExternalStrategy, LoadProfileForecastExternalStrategy)
+from gsy_e.models.strategy.external_strategies.pv import PVUserProfileExternalStrategy
+from gsy_e.models.strategy.external_strategies.smart_meter import SmartMeterExternalStrategy
+from gsy_e.models.strategy.infinite_bus import InfiniteBusStrategy
+from gsy_e.models.strategy.market_maker_strategy import MarketMakerStrategy
+from gsy_e.models.strategy.predefined_load import DefinedLoadStrategy
+from gsy_e.models.strategy.predefined_pv import PVUserProfileStrategy
+from gsy_e.models.strategy.scm.load import SCMLoadProfileStrategy
+from gsy_e.models.strategy.scm.smart_meter import SCMSmartMeterStrategy
+from gsy_e.models.strategy.smart_meter import SmartMeterStrategy
+
+
+class TestStrategyInheritance:
+    # pylint: disable=protected-access
+    @staticmethod
+    @pytest.mark.parametrize("attr_name, strategy_classes", [
+        ["daily_load_profile_uuid", [DefinedLoadStrategy,
+                                     SCMLoadProfileStrategy,
+                                     LoadProfileExternalStrategy,
+                                     LoadProfileForecastExternalStrategy]],
+        ["power_profile_uuid", [PVUserProfileStrategy,
+                                PVUserProfileExternalStrategy]],
+        ["smart_meter_profile_uuid", [SmartMeterStrategy,
+                                      SmartMeterExternalStrategy,
+                                      SCMSmartMeterStrategy]],
+        ["energy_rate_profile_uuid", [MarketMakerStrategy,
+                                      InfiniteBusStrategy]],
+        ["buying_rate_profile_uuid", [InfiniteBusStrategy]]
+
+    ])
+    def test_profile_uuids_are_part_of_strategy_attrs(attr_name, strategy_classes):
+        """
+        Makes sure that the profile_uuid are saved in the attributes
+        in order to be read by the profile handler.
+        """
+        profile_uuid = "profile_uuid"
+        for strategy_class in strategy_classes:
+            parameter_dict = {attr_name: profile_uuid}
+            strategy_class._read_or_rotate_profiles = Mock()
+            strategy = strategy_class(**parameter_dict)
+            assert getattr(strategy, attr_name) == profile_uuid
