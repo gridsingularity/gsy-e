c3fe366904c8c083e45079c9f302f225c3905a1d
Amirhossein Saemi <amirhossein@gridsingularity.com>
2022-11-28 12:01:16 +0100

    GSYE-349: Check settlement bids/offers of the ExternalStorageStrategy

--- tests/strategies/external/test_storage.py
+++ tests/strategies/external/test_storage.py
@@ -23,6 +23,8 @@ import pytest
 from gsy_framework.constants_limits import ConstSettings
 
 from gsy_e.models.strategy.external_strategies.storage import StorageExternalStrategy
+from tests.strategies.external.fixtures import (future_market_fixture,  # noqa
+                                                settlement_market_fixture)
 from tests.strategies.external.utils import (
     assert_bid_offer_aggregator_commands_return_value,
     check_external_command_endpoint_with_correct_payload_succeeds,
@@ -126,6 +128,50 @@ class TestStorageExternalStrategy:
             "your order: ['requirements', 'attributes'].")
 
     @staticmethod
+    def test_bid_aggregator_places_settlement_bid(external_storage, settlement_market):
+        unsettled_energy_kWh = 0.2
+        external_storage.area._markets.settlement_market_ids = [settlement_market.id]
+        external_storage.area._markets.settlement_markets = {
+            settlement_market.time_slot: settlement_market}
+        external_storage.state.energy_to_buy_dict[
+            settlement_market.time_slot] = unsettled_energy_kWh
+        external_storage.state.pledged_buy_kWh[settlement_market.time_slot] = 0.1
+        external_storage.state.offered_buy_kWh[settlement_market.time_slot] = 0.1
+
+        return_value = external_storage.trigger_aggregator_commands({
+            "type": "bid",
+            "price": 200,
+            "energy": 0.2,
+            "time_slot": str(settlement_market.time_slot.naive()),
+            "transaction_id": str(uuid.uuid4()),
+        })
+        assert return_value["status"] == "ready"
+        assert len(settlement_market.bids.values()) == 1
+        assert list(settlement_market.bids.values())[0].energy == unsettled_energy_kWh
+
+    @staticmethod
+    def test_bid_aggregator_fails_placing_settlement_bid_more_than_required_energy(
+            external_storage, settlement_market):
+        unsettled_energy_kWh = 0.2
+        external_storage.area._markets.settlement_market_ids = [settlement_market.id]
+        external_storage.area._markets.settlement_markets = {
+            settlement_market.time_slot: settlement_market}
+        external_storage.state.energy_to_buy_dict[
+            settlement_market.time_slot] = unsettled_energy_kWh
+        external_storage.state.pledged_buy_kWh[settlement_market.time_slot] = 0.1
+        external_storage.state.offered_buy_kWh[settlement_market.time_slot] = 0.1
+
+        return_value = external_storage.trigger_aggregator_commands({
+            "type": "bid",
+            "price": 200,
+            "energy": 0.4,
+            "time_slot": str(settlement_market.time_slot.naive()),
+            "transaction_id": str(uuid.uuid4()),
+        })
+        assert return_value["status"] == "error"
+        assert len(settlement_market.bids.values()) == 0
+
+    @staticmethod
     def test_offer_aggregator(external_storage):
         """The _offer_aggregator command succeeds."""
         external_storage.state.energy_to_sell_dict[
@@ -176,3 +222,47 @@ class TestStorageExternalStrategy:
         assert return_value["message"] == (
             "The following arguments are not supported for this market and have been removed from "
             "your order: ['requirements', 'attributes'].")
+
+    @staticmethod
+    def test_offer_aggregator_places_settlement_offer(external_storage, settlement_market):
+        unsettled_energy_kWh = 0.2
+        external_storage.area._markets.settlement_market_ids = [settlement_market.id]
+        external_storage.area._markets.settlement_markets = {
+            settlement_market.time_slot: settlement_market}
+        external_storage.state.energy_to_sell_dict[
+            settlement_market.time_slot] = unsettled_energy_kWh
+        external_storage.state.offered_sell_kWh[settlement_market.time_slot] = 0.1
+        external_storage.state.pledged_sell_kWh[settlement_market.time_slot] = 0.1
+
+        return_value = external_storage.trigger_aggregator_commands({
+            "type": "offer",
+            "price": 200,
+            "energy": 0.2,
+            "time_slot": str(settlement_market.time_slot.naive()),
+            "transaction_id": str(uuid.uuid4()),
+        })
+        assert return_value["status"] == "ready"
+        assert len(settlement_market.offers.values()) == 1
+        assert list(settlement_market.offers.values())[0].energy == unsettled_energy_kWh
+
+    @staticmethod
+    def test_offer_aggregator_fails_placing_settlement_offer_more_than_avaialble_energy(
+            external_storage, settlement_market):
+        unsettled_energy_kWh = 0.2
+        external_storage.area._markets.settlement_market_ids = [settlement_market.id]
+        external_storage.area._markets.settlement_markets = {
+            settlement_market.time_slot: settlement_market}
+        external_storage.state.energy_to_sell_dict[
+            settlement_market.time_slot] = unsettled_energy_kWh
+        external_storage.state.offered_sell_kWh[settlement_market.time_slot] = 0.1
+        external_storage.state.pledged_sell_kWh[settlement_market.time_slot] = 0.1
+
+        return_value = external_storage.trigger_aggregator_commands({
+            "type": "offer",
+            "price": 200,
+            "energy": 0.4,
+            "time_slot": str(settlement_market.time_slot.naive()),
+            "transaction_id": str(uuid.uuid4()),
+        })
+        assert return_value["status"] == "error"
+        assert len(settlement_market.offers.values()) == 0
