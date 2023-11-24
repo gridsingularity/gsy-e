463865da0a112cfeee8b949922e0427ab7fe8b63
hannesdiedrich <hannes@gridsingularity.com>
2023-11-24 14:08:59 +0100

    Deployed 2c8a01068 with MkDocs version: 1.3.0

---
+++ javascripts/mathjax.js
@@ -0,0 +1,17 @@
+window.MathJax = {
+  tex: {
+    tags: 'ams'
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
