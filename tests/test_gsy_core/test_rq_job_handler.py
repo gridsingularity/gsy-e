7aa483d0853b82da27970629fb82f19a2be6fdf3
hannesdiedrich <hannes@gridsingularity.com>
2024-03-05 11:02:24 +0100

    GSYE-675: Roll back slot length correctly in TestRqJobHandler

--- tests/test_gsy_core/test_rq_job_handler.py
+++ tests/test_gsy_core/test_rq_job_handler.py
@@ -23,10 +23,12 @@ class TestRqJobHandler:
         self.original_connect_to_profiles_db = gsy_e.constants.CONNECT_TO_PROFILES_DB
         self.original_config_id = gsy_e.constants.CONFIGURATION_ID
         self.original_start_date = GlobalConfig.start_date
+        self.original_slot_length = GlobalConfig.slot_length
 
     def teardown_method(self):
         GlobalConfig.CONFIG_TYPE = self.original_config_type
         GlobalConfig.start_date = self.original_start_date
+        GlobalConfig.slot_length = self.original_slot_length
         gsy_e.constants.EXTERNAL_CONNECTION_WEB = self.original_external_connection_web
         gsy_e.constants.RUN_IN_REALTIME = self.original_run_in_realtime
         gsy_e.constants.SEND_EVENTS_RESPONSES_TO_SDK_VIA_RQ = self.original_sertsvr
