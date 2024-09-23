6104c6df3514381e2b76a311256118fc757845d0
hannesdiedrich <hannes@gridsingularity.com>
2024-09-23 10:55:06 +0200

    GSYE-766: Fix NonP2PHandler: - adapt _is_home_area to actual scenario sent by gsy-web - correctly get the energy rates from infinite bus asset in the first level of the scenario

--- src/gsy_e/gsy_e_core/non_p2p_handler.py
+++ src/gsy_e/gsy_e_core/non_p2p_handler.py
@@ -1,4 +1,5 @@
 from gsy_framework.constants_limits import GlobalConfig
+from gsy_framework.exceptions import GSyException
 
 
 class NonP2PHandler:
@@ -6,14 +7,29 @@ class NonP2PHandler:
 
     def __init__(self, scenario: dict):
         self.non_p2p_scenario = scenario
+        self._energy_sell_rate = 0.0
+        self._energy_buy_rate = 0.0
+        self._get_energy_rates_from_infinite_bus(scenario)
         self._handle_non_p2p_scenario(scenario)
+
+    def _get_energy_rates_from_infinite_bus(self, scenario: dict):
+        for child in scenario["children"]:
+            if child.get("type") == "InfiniteBus":
+                self._energy_buy_rate = child.get("energy_buy_rate", GlobalConfig.FEED_IN_TARIFF)
+                self._energy_sell_rate = child.get(
+                    "energy_sell_rate", GlobalConfig.MARKET_MAKER_RATE
+                )
+                return
+
+        raise GSyException(
+            "For non-p2p simulation, an InfiniteBus has to be present in the first "
+            "level of the configuration tree."
+        )
 
     @staticmethod
     def _is_home_area(area: dict):
-        return (
-            area["type"] == "Area"
-            and area["children"]
-            and all(child["type"] != "Area" for child in area["children"])
+        return area.get("children") and all(
+            child.get("type", None) for child in area.get("children")
         )
 
     def _add_market_maker_to_home(self, area: dict):
@@ -25,8 +41,8 @@ class NonP2PHandler:
             {
                 "name": "MarketMaker",
                 "type": "InfiniteBus",
-                "energy_buy_rate": GlobalConfig.FEED_IN_TARIFF,
-                "energy_sell_rate": GlobalConfig.MARKET_MAKER_RATE,
+                "energy_buy_rate": self._energy_buy_rate,
+                "energy_sell_rate": self._energy_sell_rate,
             }
         )
 
--- tests/test_gsy_core/test_non_p2p_handler.py
+++ tests/test_gsy_core/test_non_p2p_handler.py
@@ -1,27 +1,27 @@
-from unittest.mock import patch
-
 from gsy_e.gsy_e_core.non_p2p_handler import NonP2PHandler
 
 SCENARIO = {
     "name": "Grid",
-    "type": "Area",
     "children": [
         {
+            "name": "InfiniteBus",
+            "type": "InfiniteBus",
+            "energy_sell_rate": 31.0,
+            "energy_buy_rate": 15.0,
+        },
+        {
             "name": "Community",
-            "type": "Area",
             "children": [
                 {
                     "name": "House 1",
-                    "type": "Area",
                     "children": [{"name": "Load", "type": "Load"}, {"name": "PV", "type": "PV"}],
                 },
                 {
                     "name": "House 2",
-                    "type": "Area",
                     "children": [{"name": "Load", "type": "Load"}, {"name": "PV", "type": "PV"}],
                 },
             ],
-        }
+        },
     ],
 }
 
@@ -29,47 +29,48 @@ SCENARIO = {
 class TestNonP2PHandler:
 
     @staticmethod
-    @patch("gsy_framework.constants_limits.GlobalConfig.MARKET_MAKER_RATE", 31)
-    @patch("gsy_framework.constants_limits.GlobalConfig.FEED_IN_TARIFF", 8)
     def test_handle_non_p2p_scenario_adds_market_makers_to_homes():
         handler = NonP2PHandler(SCENARIO)
+        print(handler.non_p2p_scenario)
         assert handler.non_p2p_scenario == {
             "name": "Grid",
-            "type": "Area",
             "children": [
                 {
+                    "name": "InfiniteBus",
+                    "type": "InfiniteBus",
+                    "energy_sell_rate": 31.0,
+                    "energy_buy_rate": 15.0,
+                },
+                {
                     "name": "Community",
-                    "type": "Area",
                     "children": [
                         {
                             "name": "House 1",
-                            "type": "Area",
                             "children": [
                                 {"name": "Load", "type": "Load"},
                                 {"name": "PV", "type": "PV"},
                                 {
                                     "name": "MarketMaker",
                                     "type": "InfiniteBus",
-                                    "energy_buy_rate": 8,
-                                    "energy_sell_rate": 31,
+                                    "energy_buy_rate": 15.0,
+                                    "energy_sell_rate": 31.0,
                                 },
                             ],
                         },
                         {
                             "name": "House 2",
-                            "type": "Area",
                             "children": [
                                 {"name": "Load", "type": "Load"},
                                 {"name": "PV", "type": "PV"},
                                 {
                                     "name": "MarketMaker",
                                     "type": "InfiniteBus",
-                                    "energy_buy_rate": 8,
-                                    "energy_sell_rate": 31,
+                                    "energy_buy_rate": 15.0,
+                                    "energy_sell_rate": 31.0,
                                 },
                             ],
                         },
                     ],
-                }
+                },
             ],
         }
