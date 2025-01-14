34e4fddb18689ff5371a230e6cb2043c8160343b
hannesdiedrich <hannes@gridsingularity.com>
2025-01-14 11:09:22 +0100

    GSYE-835: Adapt TankEnergyParameters.get_min_energy_consumption so the storage temperature can drop to its mininum

--- tests/strategies/energy_parameters/test_tank_energy_parameters.py
+++ tests/strategies/energy_parameters/test_tank_energy_parameters.py
@@ -80,7 +80,7 @@ class TestAllTanksEnergyParameters:
     def test_get_min_energy_consumption(self, cop):
         self._tanks.decrease_tanks_temp_from_heat_energy(2000, self._datetime)
         min_energy_consumption = self._tanks.get_min_energy_consumption(cop, self._datetime)
-        assert isclose(min_energy_consumption, 0.55555 / cop, rel_tol=0.0001)
+        assert isclose(min_energy_consumption, 0.1851851 / cop, rel_tol=0.0001)
 
     def test_get_average_tank_temperature(self):
         self._tanks._tanks_energy_parameters[0]._state._storage_temp_C[self._datetime] = 30
