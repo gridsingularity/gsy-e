7c5b448687e1a3b66b7d7f726491a06fccae4ac5
spyrostz <spyros@gridsingularity.com>
2025-04-14 08:38:35 +0300

    GSYE-864: Corrected unit test for heat pump tanks.

--- tests/strategies/test_heat_pump.py
+++ tests/strategies/test_heat_pump.py
@@ -328,8 +328,8 @@ class TestHeatPumpStrategy:
         strategy = heatpump_fixture[0]
         strategy.preferred_buying_rate = 15
         tank_parameters = strategy._energy_params._state.tanks._tanks_states[0]
-        tank_parameters._parameters.min_temp_C = 10
-        tank_parameters._state.get_storage_temp_C = MagicMock(return_value=20)
+        tank_parameters._min_storage_temp_C = 10
+        tank_parameters.get_storage_temp_C = MagicMock(return_value=20)
         tank_parameters.get_temp_decrease = MagicMock(return_value=10)
         strategy._energy_params.get_min_energy_demand_kWh = MagicMock(return_value=1)
         strategy._energy_params.get_max_energy_demand_kWh = MagicMock(return_value=2)
