9cb19912437f2efd7f70fa19b4ee5e5278d062e2
hannesdiedrich <hannes@gridsingularity.com>
2026-01-27 14:34:10 +0100

    GSYE-901: Refactor IndividualCOPModel in order to reduce code duplication

--- src/gsy_e/models/strategy/energy_parameters/heatpump/cop_models/cop_models.py
+++ src/gsy_e/models/strategy/energy_parameters/heatpump/cop_models/cop_models.py
@@ -69,117 +69,6 @@ class IndividualCOPModel(BaseCOPModel):
             self._model = json.load(fp)
         self.model_type = model_type
 
-    def _calc_power(self, source_temp_C: float, condenser_temp_C: float, heat_demand_kW: float):
-        CAPFT = (
-            self._model["CAPFT"][0]
-            + self._model["CAPFT"][1] * source_temp_C
-            + self._model["CAPFT"][3] * source_temp_C**2
-            + self._model["CAPFT"][2] * condenser_temp_C
-            + self._model["CAPFT"][5] * condenser_temp_C**2
-            + self._model["CAPFT"][4] * source_temp_C * condenser_temp_C
-        )
-
-        HEIRFT = (
-            self._model["HEIRFT"][0]
-            + self._model["HEIRFT"][1] * source_temp_C
-            + self._model["HEIRFT"][3] * source_temp_C**2
-            + self._model["HEIRFT"][2] * condenser_temp_C
-            + self._model["HEIRFT"][5] * condenser_temp_C**2
-            + self._model["HEIRFT"][4] * source_temp_C * condenser_temp_C
-        )
-
-        # Partial Load Ratio (PLR)
-        PLR = heat_demand_kW / (self._model["Qref"] * CAPFT)
-
-        # HEIRFPLR calculation
-        HEIRFPLR = (
-            self._model["HEIRFPLR"][0]
-            + self._model["HEIRFPLR"][1] * PLR
-            + self._model["HEIRFPLR"][2] * PLR**2
-        )
-
-        # Power consumption (P) calculation
-        return self._model["Pref"] * CAPFT * HEIRFT * HEIRFPLR
-
-    def _resolve_heat(
-        self, source_temp_C: float, condenser_temp_C: float, electricity_demand_kW: float
-    ):
-        CAPFT = (
-            self._model["CAPFT"][0]
-            + self._model["CAPFT"][1] * source_temp_C
-            + self._model["CAPFT"][3] * source_temp_C**2
-            + self._model["CAPFT"][2] * condenser_temp_C
-            + self._model["CAPFT"][5] * condenser_temp_C**2
-            + self._model["CAPFT"][4] * source_temp_C * condenser_temp_C
-        )
-
-        HEIRFT = (
-            self._model["HEIRFT"][0]
-            + self._model["HEIRFT"][1] * source_temp_C
-            + self._model["HEIRFT"][3] * source_temp_C**2
-            + self._model["HEIRFT"][2] * condenser_temp_C
-            + self._model["HEIRFT"][5] * condenser_temp_C**2
-            + self._model["HEIRFT"][4] * source_temp_C * condenser_temp_C
-        )
-
-        # Partial Load Ratio (PLR)
-        Q = sp.symbols("Q")
-        PLR = Q / (self._model["Qref"] * CAPFT)
-
-        # HEIRFPLR calculation
-        HEIRFPLR = (
-            self._model["HEIRFPLR"][0]
-            + self._model["HEIRFPLR"][1] * PLR
-            + self._model["HEIRFPLR"][2] * PLR**2
-        )
-
-        solutions = sp.solve(
-            sp.Eq(electricity_demand_kW, self._model["Pref"] * CAPFT * HEIRFT * HEIRFPLR), Q
-        )
-
-        return self._select_Q_solution(solutions, CAPFT)
-
-    def _select_Q_solution(self, Q_solutions, CAPFT) -> Optional[float]:
-        """
-        Selects the correct Q and PLR solution based on:
-        - PLR = Q / (Qref * fCAPFT)
-        - both PLRs must be between 0 and 1
-        - the correct branch is the one with the LARGER PLR
-          (as indicated by the training dataset)
-        """
-        PLR_dict = {
-            q / (self._model["Qref"] * CAPFT): q
-            for q in Q_solutions
-            if isinstance(q, sp.core.numbers.Float) and 0 <= q / (self._model["Qref"] * CAPFT) <= 1
-        }
-        if not PLR_dict:
-            PLR_list = [q / (self._model["Qref"] * CAPFT) for q in Q_solutions]
-            log.error(
-                "IndividualCOPModel: No physically feasible PLR solutions Q: %s, PLR: %s ",
-                Q_solutions,
-                PLR_list,
-            )
-            return None
-        return float(PLR_dict[max(PLR_dict)])
-
-    def _limit_heat_demand_kW(self, heat_demand_kW: float) -> float:
-        assert heat_demand_kW is not None, "heat demand should be provided"
-        if heat_demand_kW > self._model["Q_max"]:
-            log.error(
-                "calc_cop: heat demand (%s kW) exceeds maximum heat_demand_kW: %s",
-                heat_demand_kW,
-                self._model["Q_max"],
-            )
-            return self._model["Q_max"]
-        if heat_demand_kW < self._model["Q_min"]:
-            log.error(
-                "calc_cop: heat demand (%s kW) exceeds minimum heat_demand_kW: %s",
-                heat_demand_kW,
-                self._model["Q_min"],
-            )
-            return self._model["Q_min"]
-        return heat_demand_kW
-
     def calc_q_from_p_kW(
         self,
         source_temp_C: float,
@@ -228,6 +117,105 @@ class IndividualCOPModel(BaseCOPModel):
                 round(electrical_power_kW, 2),
             )
         return cop
+
+    def _capft(self, source_temp_C: float, condenser_temp_C: float):
+        return (
+            self._model["CAPFT"][0]
+            + self._model["CAPFT"][1] * source_temp_C
+            + self._model["CAPFT"][3] * source_temp_C**2
+            + self._model["CAPFT"][2] * condenser_temp_C
+            + self._model["CAPFT"][5] * condenser_temp_C**2
+            + self._model["CAPFT"][4] * source_temp_C * condenser_temp_C
+        )
+
+    def _heirft(self, source_temp_C: float, condenser_temp_C: float):
+        return (
+            self._model["HEIRFT"][0]
+            + self._model["HEIRFT"][1] * source_temp_C
+            + self._model["HEIRFT"][3] * source_temp_C**2
+            + self._model["HEIRFT"][2] * condenser_temp_C
+            + self._model["HEIRFT"][5] * condenser_temp_C**2
+            + self._model["HEIRFT"][4] * source_temp_C * condenser_temp_C
+        )
+
+    def _heirfplr(self, plr):
+        return (
+            self._model["HEIRFPLR"][0]
+            + self._model["HEIRFPLR"][1] * plr
+            + self._model["HEIRFPLR"][2] * plr**2
+        )
+
+    def _calc_power(self, source_temp_C: float, condenser_temp_C: float, heat_demand_kW: float):
+        CAPFT = self._capft(source_temp_C, condenser_temp_C)
+        PLR = heat_demand_kW / (self._model["Qref"] * CAPFT)
+
+        return (
+            self._model["Pref"]
+            * CAPFT
+            * self._heirft(source_temp_C, condenser_temp_C)
+            * self._heirfplr(PLR)
+        )
+
+    def _resolve_heat(
+        self, source_temp_C: float, condenser_temp_C: float, electricity_demand_kW: float
+    ):
+        CAPFT = self._capft(source_temp_C, condenser_temp_C)
+        Q = sp.symbols("Q")
+        PLR = Q / (self._model["Qref"] * CAPFT)
+
+        solutions = sp.solve(
+            sp.Eq(
+                electricity_demand_kW,
+                self._model["Pref"]
+                * CAPFT
+                * self._heirft(source_temp_C, condenser_temp_C)
+                * self._heirfplr(PLR),
+            ),
+            Q,
+        )
+
+        return self._select_Q_solution(solutions, CAPFT)
+
+    def _select_Q_solution(self, Q_solutions, CAPFT) -> Optional[float]:
+        """
+        Selects the correct Q and PLR solution based on:
+        - PLR = Q / (Qref * fCAPFT)
+        - both PLRs must be between 0 and 1
+        - the correct branch is the one with the LARGER PLR
+          (as indicated by the training dataset)
+        """
+        PLR_dict = {
+            q / (self._model["Qref"] * CAPFT): q
+            for q in Q_solutions
+            if isinstance(q, sp.core.numbers.Float) and 0 <= q / (self._model["Qref"] * CAPFT) <= 1
+        }
+        if not PLR_dict:
+            PLR_list = [q / (self._model["Qref"] * CAPFT) for q in Q_solutions]
+            log.error(
+                "IndividualCOPModel: No physically feasible PLR solutions Q: %s, PLR: %s ",
+                Q_solutions,
+                PLR_list,
+            )
+            return None
+        return float(PLR_dict[max(PLR_dict)])
+
+    def _limit_heat_demand_kW(self, heat_demand_kW: float) -> float:
+        assert heat_demand_kW is not None, "heat demand should be provided"
+        if heat_demand_kW > self._model["Q_max"]:
+            log.error(
+                "calc_cop: heat demand (%s kW) exceeds maximum heat_demand_kW: %s",
+                heat_demand_kW,
+                self._model["Q_max"],
+            )
+            return self._model["Q_max"]
+        if heat_demand_kW < self._model["Q_min"]:
+            log.error(
+                "calc_cop: heat demand (%s kW) exceeds minimum heat_demand_kW: %s",
+                heat_demand_kW,
+                self._model["Q_min"],
+            )
+            return self._model["Q_min"]
+        return heat_demand_kW
 
 
 class UniversalCOPModel(BaseCOPModel):
